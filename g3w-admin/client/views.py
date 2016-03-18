b431add27df67ef1a9557e097f0aeea5fcf394a2
wlorenzetti <lorenzetti@gis3w.it>
2016-03-18 14:32:13 +0100

    Add initproject to group serialization.

--- g3w-admin/client/views.py
+++ g3w-admin/client/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
 from django.http import Http404
-from django.apps import apps
+from django.core.urlresolvers import reverse
 from rest_framework.renderers import JSONRenderer
 from core.api.serializers import GroupSerializer, Group
 
@@ -15,8 +15,10 @@ class ClientView(TemplateView):
 
         # group serializer
         group = get_object_or_404(Group, slug=kwargs['group_slug'])
-        groupSerializer = GroupSerializer(group)
-        contextData['group_config'] = 'var config ={{ "baseUrl":"{}", "group":{} }}'.format('', JSONRenderer().render(groupSerializer.data))
+        groupSerializer = GroupSerializer(group,projectId=kwargs['project_id'])
+
+        # add baseUrl property
+        contextData['group_config'] = 'var config ={{ "baseUrl":"{}", "group":{} }}'.format(reverse('ows', kwargs=kwargs), JSONRenderer().render(groupSerializer.data))
 
         # project by type(app)
         if not '{}-{}'.format(kwargs['project_type'],kwargs['project_id']) in groupSerializer.projects.keys():
--- g3w-admin/core/api/serializers.py
+++ g3w-admin/core/api/serializers.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.apps import apps
 from rest_framework import serializers
+from rest_framework.fields import empty
 from core.models import Group
 
 
@@ -9,6 +10,11 @@ class GroupSerializer(serializers.ModelSerializer):
     minscale = serializers.IntegerField(source='min_scale', read_only=True)
     maxscale = serializers.IntegerField(source='max_scale', read_only=True)
     crs = serializers.IntegerField(source='srid', read_only=True)
+
+    def __init__(self,instance=None, data=empty, **kwargs):
+        self.projectId = kwargs['projectId']
+        del(kwargs['projectId'])
+        super(GroupSerializer, self).__init__(instance, data, **kwargs)
 
     def to_representation(self, instance):
         ret = super(GroupSerializer, self).to_representation(instance)
@@ -32,7 +38,9 @@ class GroupSerializer(serializers.ModelSerializer):
         ret['baseLayers'] = []
 
         # add initproject and overviewproject
-        ret['initproject'] = None
+        ret['initproject'] = int(self.projectId)
+
+        # todo: set project id for overviewmap
         ret['overviewproject'] = None
 
         return ret
