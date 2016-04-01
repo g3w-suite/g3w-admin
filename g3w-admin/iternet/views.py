542709b46f501f30ec460e68d97b9c93f792935b
wlorenzetti <lorenzetti@gis3w.it>
2016-04-01 17:03:34 +0200

    Continue develop iternet module.

--- g3w-admin/iternet/views.py
+++ g3w-admin/iternet/views.py
@@ -1,10 +1,16 @@
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, View
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.conf import settings
+import copy
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import ElementoStradale, Config
+from .models import ElementoStradale, Config, Accesso, ElementoStradale, GiunzioneStradale
 from .forms import ConfigForm
 from .api.serializers import ElementoStradaleGeoSerializer
+from qdjango.utils.data import QgisPgConnection
+
+iternet_connection = copy.copy(settings.DATABASES[settings.ITERNET_DATABASE])
 
 class ElementoStradaleApiView(APIView):
     """
@@ -22,6 +28,19 @@ class ElementoStradaleApiView(APIView):
 class DashboardView(TemplateView):
 
     template_name = 'iternet/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+
+        # set data iternet connection
+        context['db_connection'] = iternet_connection
+
+        # get report data from nodi accessi and elementi stradali
+        context['n_accessi'] = len(Accesso.objects.all())
+        context['n_elementi_stradali'] = len(ElementoStradale.objects.all())
+        context['n_giunzioni_stradali'] = len(GiunzioneStradale.objects.all())
+
+        return context
 
 
 class ConfigView(FormView):
@@ -50,3 +69,20 @@ class ConfigView(FormView):
         return super(ConfigView, self).form_valid(form)
 
 
+class DownloadQgisPgConnectionView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        qgisPgConnection = QgisPgConnection(
+            host=iternet_connection['HOST'],
+            username=iternet_connection['USER'],
+            password=iternet_connection['PASSWORD'],
+            port=iternet_connection['PORT'],
+            name='G3W-ADMIN-ITERNET',
+            database=iternet_connection['NAME'],
+        )
+
+        # get configuration data
+        response = HttpResponse(qgisPgConnection.asXML(), content_type='text/xml')
+        response['Content-Disposition'] = 'attachment; filename="g3w_iternet_qgis_connection.xml"'
+        return response
