2207df3d0c55b35771eefa8ee73dd6f1ef1ca4b7
wlorenzetti <lorenzetti@gis3w.it>
2016-05-23 16:41:10 +0200

    Remove cdu for get as submodule.

--- g3w-admin/cdu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
-from .views import *
-
-urlpatterns = [
-    url(r'^config/$', login_required(CduConfigList.as_view()), name='cdu-config-list'),
-    url(r'config/add/$', login_required(CduConfigWizardView.as_view()), name='cdu-config-add'),
-]
