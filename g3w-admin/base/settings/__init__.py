3fe771123dc4fb5f8291db34eee6740346c50ab7
wlorenzetti <lorenzetti@gis3w.it>
2017-03-21 15:03:10 +0100

    cu

--- g3w-admin/base/settings/__init__.py
+++ g3w-admin/base/settings/__init__.py
@@ -1,7 +1,7 @@
 from .base import *
 from .base_layout_settings import *
 from .base_geo_settings import *
-from .local_settings_docker import *
+from .local_settings import *
 
 G3WADMIN_PROJECT_APPS = G3WADMIN_PROJECT_APPS + G3WADMIN_PROJECT_APPS_BASE
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + G3WADMIN_APPS + G3WADMIN_PROJECT_APPS
