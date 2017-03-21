2cb65ed6f0dd51280650d252104cb962ed5966dc
wlorenzetti <lorenzetti@gis3w.it>
2017-03-21 14:34:21 +0100

    Add license COPYING file.

---
+++ g3w-admin/base/settings/local_settings_docker.py
@@ -0,0 +1,25 @@
+import os
+
+G3WADMIN_PROJECT_APPS = [
+    'ogc'
+]
+
+G3WADMIN_LOCAL_MORE_APPS = os.environ.get('G3WSUITE_MORE_APPS', 'fronted').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('G3WSUITE_DATABASE_NAME', 'g3w_admin'),
+        'USER': os.environ.get('G3WSUITE_DATABASE_USER', 'postgres'),
+        'PASSWORD': os.environ.get('G3WSUITE_DATABASE_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('G3WSUITE_DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('G3WSUITE_DATABASE_PORT', '5432'),
+    },
+
+}
+
+DEBUG = True
+FRONTEND = True
+FRONTEND_APP = 'frontend'
+
+
