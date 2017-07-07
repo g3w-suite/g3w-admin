f13ea6e6191e2262f9e82296b89221eb59fd08ed
wlorenzetti <lorenzetti@gis3w.it>
2017-07-07 15:37:59 +0200

    cu

--- docker/install-modules.sh
+++ docker/install-modules.sh
@@ -9,7 +9,7 @@ repo["ogc"]="g3w-admin-ogc.git"
 repo["notes"]="g3w-admin-notes.git"
 repo["cdu"]="g3w-admin-cdu.git"
 repo["law"]="g3w-admin-law.git"
-repo["cadastre"]="g3w-admin-notes.git"
+repo["cadastre"]="g3w-admin-cadastre.git"
 repo["iternet"]="g3w-admin-iternet.git"
 
 for key in ${!repo[@]}; do
