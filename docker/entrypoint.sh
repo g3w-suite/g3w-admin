02b9616f1d8b772a29b2a9d8b1a932143ce11266
wlorenzetti <lorenzetti@gis3w.it>
2017-07-11 12:02:31 +0200

    cu

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 # Replace the UWSGI uri in the Nginx tcp proxy configuration.
-#if [ -n "$BASEURL" ]; then
-	#sed -e "s/{{BASEURL}}/$BASEURL/" /home/timon/nginx.conf.tpl > /etc/nginx/sites-available/default
-#else
-	#echo "ERROR - Must specify: -e BASEURL=<baseurl>"
-#	exit 1
+sed -e "s/{{BASEURL}}/$BASEURL/g" /home/g3wsuite/nginx.conf.tpl > /etc/nginx/sites-available/default
+
 
 python /home/g3wsuite/g3w-admin/g3w-admin/manage.py migrate
 python /home/g3wsuite/g3w-admin/g3w-admin/manage.py collectstatic --noinput
