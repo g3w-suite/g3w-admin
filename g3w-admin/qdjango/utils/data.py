92b20e0d0504657d6f39711fea475a0196eaca29
wlorenzetti <lorenzetti@gis3w.it>
2017-09-27 16:03:44 +0200

    cu: fix on group and project editor direct permitions.

--- g3w-admin/qdjango/utils/data.py
+++ g3w-admin/qdjango/utils/data.py
@@ -603,7 +603,8 @@ class QgisProject(XmlData):
 
             # we have to rewind the underlying file in case it has been already parsed
             self.qgisProjectFile.file.seek(0)
-            self.qgisProjectTree = lxml.parse(self.qgisProjectFile, forbid_entities=False, encoding='UTF-8')
+            parser = etree.XMLParser(encoding='UTF-8')
+            self.qgisProjectTree = lxml.parse(self.qgisProjectFile, parser=parser, forbid_entities=False)
         except Exception as e:
             raise Exception(_('The project file is malformed: {}'.format(e.message)))
 
