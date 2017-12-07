ce108476fd66797821a76a8c306a4ebe62d4d30e
wlorenzetti <lorenzetti@gis3w.it>
2017-12-07 18:05:47 +0100

    cu

--- g3w-admin/client/api/views.py
+++ g3w-admin/client/api/views.py
@@ -29,7 +29,8 @@ class ClientConfigApiView(APIView):
         # signal after serialization project
         ps_data = ps.data
         for singnal_receiver, data in post_serialize_project.send(ps, app_name=project_type):
-            update_serializer_data(ps_data, data)
+            if data:
+                update_serializer_data(ps_data, data)
 
         return Response(ps.data)
 
