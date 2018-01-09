7cd95979d0ebfc5ac27a7d3e31332104a91df17a
wlorenzetti <lorenzetti@gis3w.it>
2018-01-09 16:15:36 +0100

    cu

--- g3w-admin/base/settings/base.py
+++ g3w-admin/base/settings/base.py
@@ -98,8 +98,7 @@ ROOT_URLCONF = 'base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, '../templates')],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
