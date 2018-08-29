7a1d03cfc6a71b7e5de0bdade518147002512644
Walter Lorenzetti <lorenzetti@gis3w.it>
2018-08-29 15:40:30 +0200

    CU

--- g3w-admin/core/utils/__init__.py
+++ g3w-admin/core/utils/__init__.py
@@ -3,3 +3,15 @@ def unicode2ascii(ustr):
     From unicode string return ascii encode string
     """
     return ustr.encode('ascii', errors='backslashreplace')
+
+import magic
+
+def file_path_mime(file_path):
+    """
+    get mime_type from file type
+    from: https://medium.com/@ajrbyers/file-mime-types-in-django-ee9531f3035b
+    :param file_path:
+    :return:
+    """
+    mime = magic.from_file(file_path, mime=True)
+    return mime
