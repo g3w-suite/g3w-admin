316f0cf45685d827cbf2a7bd9389bb47d70fb70f
Alessandro Pasotti <elpaso@itopen.it>
2019-07-24 16:31:40 +0200

    Rule validation

--- g3w-admin/constraints/models.py
+++ g3w-admin/constraints/models.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import Group, User
-from django.core.exceptions import ValidationError
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from django.db.models import Q
+import logging
 
+from django.contrib.auth.models import Group, User
+from django.contrib.gis.db.models.fields import GeometryField
+from django.contrib.gis.geos import MultiPolygon
+from django.core.exceptions import ValidationError
+from django.db import connection, models, transaction
+from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _
+
+from core.utils.models import get_creator_from_qdjango_layer
 from qdjango.models import Layer
+
+logger = logging.getLogger(__name__)
 
 """"Constraints module models
 
@@ -23,7 +30,7 @@ __copyright__ = 'Copyright 2019, Gis3w'
 
 
 class Constraint(models.Model):
-    """Main Constraint class: links together two layers, the editing layer and the constraint layer.
+    """Main Constraint class. Links together two layers: the editing layer and the constraint layer.
     """
 
     active = models.BooleanField(default=True)
@@ -31,6 +38,18 @@ class Constraint(models.Model):
         Layer, on_delete=models.CASCADE, related_name='editing_layer')
     constraint_layer = models.ForeignKey(
         Layer, on_delete=models.CASCADE, related_name='constraint_layer')
+
+    def clean(self):
+        """Make sure the layer is either PG or SL and check that constraint layer is Polygon"""
+
+        if self.editing_layer.layer_type not in ('spatialite', 'postgres') or self.constraint_layer.layer_type not in ('spatialite', 'postgres'):
+            raise ValidationError(
+                _('Layers types must be spatialite or postgres'))
+
+        if 'Polygon' not in self.constraint_layer.geometrytype:
+           raise ValidationError(
+                _('Constraint layer geometry type must be Polygon or MultiPolygon'))
+
 
     def __str__(self):
         return "%s, %s" % (self.editing_layer, self.constraint_layer)
@@ -42,14 +61,15 @@ class Constraint(models.Model):
 
 
 class ConstraintRule(models.Model):
-    """ Constraint rule class: links the constraint with a user or a group and define the constraint rule
-    """
+    """Constraint rule class: links the constraint with a user or a group and
+    defines the constraint SQL rule"""
+
     constraint = models.ForeignKey(Constraint, on_delete=models.CASCADE)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, null=True)
     group = models.ForeignKey(
         Group, on_delete=models.CASCADE, blank=True, null=True)
-    rule = models.TextField(_("SQL constraint rule"), max_length=255)
+    rule = models.TextField(_("SQL WHERE clause for the constraint layer"), max_length=255)
 
     def __str__(self):
         return "%s, %s: %s" % (self.constraint, self.user_or_group, self.rule)
@@ -58,6 +78,7 @@ class ConstraintRule(models.Model):
         managed = True
         verbose_name = _('Constraint rule')
         verbose_name_plural = _('Constraint rules')
+        unique_together = (('constraint', 'user'), ('constraint', 'group'))
 
     @property
     def user_or_group(self):
@@ -74,7 +95,79 @@ class ConstraintRule(models.Model):
             raise ValidationError(
                 _('You cannot define a user and a group at the same time'))
 
-        # TODO: SQL validator (probably it goes into an independent method to be reused in the API)
+        if not self.group and not self.user:
+            raise ValidationError(
+                _('You must define a user OR a group'))
+
+        # TODO: SQL validator (it probably goes into an independent method validate_sql to be reused in the API)
+
+    def get_constraint_geometry(self):
+        """Returns the geometry from the constraint layer and rule
+
+        :return: the constraint geometry and the number of matched records
+        :rtype: tuple( MultiPolygon, integer)
+        """
+
+        editing_model_creator = get_creator_from_qdjango_layer(self.constraint.editing_layer)
+        editing_geom_field = [k for k,v in editing_model_creator.django_model_fields.items() if isinstance(v, GeometryField)][0]
+        constraint_model_creator = get_creator_from_qdjango_layer(self.constraint.constraint_layer)
+        constraint_geom_field = [k for k,v in constraint_model_creator.django_model_fields.items() if isinstance(v, GeometryField)][0]
+        constraint_pk_field = constraint_model_creator.geo_model._meta.pk.name
+        # Get SRID of editing field and transform (it should be a NOOP in case it is already in the right SRID)
+        editing_srid = editing_model_creator.django_model_fields[editing_geom_field].srid
+        sql = 'SELECT %s, ST_AsBinary(ST_Transform(%s, %s)) FROM %s WHERE %s' % (
+            constraint_pk_field,
+            constraint_geom_field,
+            editing_srid,
+            constraint_model_creator.table,
+            self.rule
+        )
+        # Actually fetch the constraint data
+        constraint_geometry = MultiPolygon()
+        constraint_geometry.set_srid(editing_srid)
+        matched_counter = 0
+        with transaction.atomic():
+            for record in constraint_model_creator.geo_model.objects.raw(sql):
+                constraint_geometry.append(getattr(record, constraint_geom_field))
+                matched_counter += 1
+        return constraint_geometry, matched_counter
+
+    def get_query_set(self):
+        """Returns the query set from the editing layer filtered by the rule"""
+
+        editing_model_creator = get_creator_from_qdjango_layer(self.constraint.editing_layer)
+        editing_geom_field = [k for k,v in editing_model_creator.django_model_fields.items() if isinstance(v, GeometryField)][0]
+
+        constraint_geometry, matched_counter = self.get_constraint_geometry()
+
+        # Now fetch data from editing layer
+        # TODO: check if we want "intersects" instead
+        filter_key = editing_geom_field + '__within'
+        filters = {
+            filter_key: constraint_geometry
+        }
+        # Apparently, in spatialite:
+        # select st_within(st_geomfromtext('point( 9 9)'), st_geomfromtext('multipolygon empty'));
+        # returns -1 that is interpreted as TRUE, so we invert the filter to return an empty query set
+        if constraint_geometry.empty and self.constraint.editing_layer.layer_type == 'spatialite':
+            return editing_model_creator.geo_model.objects.exclude(**filters)
+        else:
+            return editing_model_creator.geo_model.objects.filter(**filters)
+
+
+    def validate_sql(self):
+        """Checks if the rule can be executed without errors
+
+        :return: True if rule has valid SQL
+        :rtype: boolean
+        """
+
+        try:
+            self.get_query_set()
+        except Exception as ex:
+            logger.debug('Validate SQL failed: %s' % ex)
+            return False
+        return True
 
     @classmethod
     def constraints_for_user(cls, user, editing_layer):
@@ -96,5 +189,3 @@ class ConstraintRule(models.Model):
             return cls.objects.filter(Q(constraint__in=constraints), Q(user=user)|Q(group__in=user_groups))
         else:
             return cls.objects.filter(constraint__in=constraints, user=user)
-
-
--- g3w-admin/constraints/tests/test_constraints.py
+++ g3w-admin/constraints/tests/test_constraints.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+import os
 
-# Create your tests here.
+from django.contrib.auth.models import Group as UserGroup
+from django.core.exceptions import ValidationError
+from django.core.files import File
+from django.db import IntegrityError, transaction
+from django.test import TestCase, override_settings
 
 from constraints.models import *
+from core.models import G3WSpatialRefSys, Group
+from qdjango.models import Project
+from qdjango.utils.data import QgisProject
 
 """"Tests for constraints module API
 
@@ -19,15 +26,6 @@ __date__ = '2019-07-19'
 __copyright__ = 'Copyright 2019, Gis3w'
 
 
-from django.test import TestCase, override_settings
-from django.core.files import File
-from core.models import Group, G3WSpatialRefSys
-from qdjango.models import Project
-from qdjango.utils.data import QgisProject
-from django.core.exceptions import ValidationError
-from django.contrib.auth.models import Group as UserGroup
-
-import os
 
 CURRENT_PATH = os.getcwd()
 TEST_BASE_PATH = '/constraints/tests/data/'
@@ -66,6 +64,11 @@ class ConstraintsTests(TestCase):
         cls.project.save()
         qgis_project_file.close()
 
+    def tearDown(self):
+        """Delete all test data"""
+
+        Constraint.objects.all().delete()
+
 
     def test_create_constraint(self):
         """Test constraints creation"""
@@ -73,7 +76,20 @@ class ConstraintsTests(TestCase):
         editing_layer = Layer.objects.get(name='editing_layer')
         constraint_layer = Layer.objects.get(name='constraint_layer')
         constraint = Constraint(editing_layer=editing_layer, constraint_layer=constraint_layer)
+        # Test validation
+        constraint.clean()
         constraint.save()
+
+        # Check layer types (PG or SL)
+        with self.assertRaises(ValidationError) as ex:
+            Constraint(editing_layer=editing_layer, constraint_layer=Layer(layer_type='GDAL')).clean()
+
+        with self.assertRaises(ValidationError) as ex:
+            Constraint(editing_layer=Layer(layer_type='GDAL'), constraint_layer=constraint_layer).clean()
+
+        # Check if constraints layer is polygon
+        with self.assertRaises(ValidationError) as ex:
+            Constraint(editing_layer=constraint_layer, constraint_layer=editing_layer).clean()
 
         rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='int_f=1')
         rule.save()
@@ -95,7 +111,76 @@ class ConstraintsTests(TestCase):
         self.assertEqual(len(rules), 1)
         self.assertEqual(rules[0], rule3)
 
+        # Test we need a user OR a group
+        with self.assertRaises(ValidationError) as ex:
+            rule4 = ConstraintRule(constraint=constraint, rule='int_f=1')
+            rule4.clean()
+
         # Test we get nothing for the other layer and user
         rules = ConstraintRule.constraints_for_user(self.test_user3, editing_layer)
         rules = ConstraintRule.constraints_for_user(self.test_user2, constraint_layer)
         self.assertEqual(len(rules), 0)
+
+
+
+    def test_unique(self):
+        """Check unique together conditions"""
+
+        editing_layer = Layer.objects.get(name='editing_layer')
+        constraint_layer = Layer.objects.get(name='constraint_layer')
+        constraint = Constraint(editing_layer=editing_layer, constraint_layer=constraint_layer)
+        constraint.save()
+
+        rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='int_f=1')
+        rule.save()
+
+        # Check unique_together
+        with transaction.atomic():
+            with self.assertRaises(IntegrityError) as ex:
+                rule_duplicate = ConstraintRule(constraint=constraint, user=self.test_user1, rule='int_f=1')
+                rule_duplicate.save()
+
+        rule3 = ConstraintRule(constraint=constraint, group=self.group, rule='int_f=1')
+        rule3.save()
+        with transaction.atomic():
+            with self.assertRaises(IntegrityError) as ex:
+                rule3_duplicate = ConstraintRule(constraint=constraint, group=self.group, rule='int_f=1')
+                rule3_duplicate.save()
+
+    def test_sql_validation(self):
+        """Test SQL rule validation"""
+
+        editing_layer = Layer.objects.get(name='editing_layer')
+        constraint_layer = Layer.objects.get(name='constraint_layer')
+        constraint = Constraint(editing_layer=editing_layer, constraint_layer=constraint_layer)
+        constraint.save()
+        rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='int_f=1')
+        self.assertTrue(rule.validate_sql())
+
+        rule.rule = 'not_exists=999'
+        self.assertFalse(rule.validate_sql())
+
+    def test_get_query_set(self):
+        """Test get query set"""
+
+        editing_layer = Layer.objects.get(name='editing_layer')
+        constraint_layer = Layer.objects.get(name='constraint_layer')
+        constraint = Constraint(editing_layer=editing_layer, constraint_layer=constraint_layer)
+        constraint.save()
+        rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='name=\'bagnolo\'')
+        qs = rule.get_query_set()
+        self.assertEqual(list(qs.values_list('name', flat=True)), ['bagnolo 1', 'bagnolo 2'])
+        rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='name=\'pinerolo\'')
+        qs = rule.get_query_set()
+        self.assertEqual(list(qs.values_list('name', flat=True)), ['pinerolo 3', 'pinerolo 4'])
+
+        rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='int_f=1')
+        qs = rule.get_query_set()
+        self.assertEqual(list(qs.values_list('name', flat=True)), ['bagnolo 1', 'bagnolo 2'])
+        rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='int_f=2')
+        qs = rule.get_query_set()
+        self.assertEqual(list(qs.values_list('name', flat=True)), ['pinerolo 3', 'pinerolo 4'])
+
+        rule = ConstraintRule(constraint=constraint, user=self.test_user1, rule='int_f=999')
+        qs = rule.get_query_set()
+        self.assertEqual(list(qs.values_list('name', flat=True)), [])
