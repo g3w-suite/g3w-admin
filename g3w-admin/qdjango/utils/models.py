df763b0fed0a85ee7fb9d0b9a09d4b3c2fc9afe4
Walter Lorenzetti <lorenzetti@gis3w.it>
2019-12-04 08:47:46 +0100

    Add new widget compair based on subobjet of orign datasource.

---
+++ g3w-admin/qdjango/utils/models.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""
+    Qdjango module utilities methods.
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the Mozilla Public License 2.0.
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2019-11-29'
+__copyright__ = 'Copyright 2019, GIS3W'
+
+from django.db.models import Q
+
+
+def get_widgets4layer(layer):
+    """
+    Return widgets list for qdjango layer instance
+    :param layer: Qdjango Layer model instance
+    :return: List or Querydict fo Widget models
+    """
+    from .structure import datasource2dict
+    from qdjango.models import Widget
+
+    # different by layer type
+    # for postgis layer
+    if layer.layer_type == 'postgres':
+        ds = datasource2dict(layer.datasource)
+        to_contain = Q(datasource__contains=u'dbname=\'{}\''.format(ds['dbname'])) & \
+                     Q(datasource__contains=u'host={}'.format(ds['host'])) & \
+                     Q(datasource__contains=u'table={}'.format(ds['table']))
+        return Widget.objects.filter(to_contain)
+    else:
+        return Widget.objects.filter(datasource=layer.datasource)
