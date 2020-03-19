31949fa3cfce3ab733767890326ada39eb6183b4
wlorenzetti <lorenzetti@gis3w.it>
2020-03-19 11:03:01 +0100

    Add url namespaces for reverse

--- g3w-admin/OWS/urls.py
+++ g3w-admin/OWS/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import *
 
+app_name = 'OWS'
+
 urlpatterns = [
     # url working for qgis wms client
     url(r'^ows/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_id>[-_\w\d]+)/&$', OWSView.as_view(),
