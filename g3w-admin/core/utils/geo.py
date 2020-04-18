e0255db1606641c75f9aa9c17532704b3dce5f7a
wlorenzetti <lorenzetti@gis3w.it>
2020-04-18 16:11:39 +0200

    Remove TransformBBox utils function.

--- g3w-admin/core/utils/geo.py
+++ g3w-admin/core/utils/geo.py
@@ -1,13 +1,5 @@
 from pyproj import Proj, transform
 
-def TransformBBox(epsgFrom, epsgTo, BBox):
-
-    prjFrom = Proj(init="epsg:".format(epsgFrom))
-    prjTo = Proj(init="epsg:".format(epsgTo))
-
-    # Todo: implement bbox split and transform
-
-
 def camel_geometry_type(geometry_type):
     """
     From geometry type case insensitive to standard name
