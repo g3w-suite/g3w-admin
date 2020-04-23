5e435f63355dea0623671cda8738181de5939f2c
wlorenzetti <lorenzetti@gis3w.it>
2020-04-23 11:31:07 +0200

    Update doc.

--- g3w-admin/core/utils/response.py
+++ g3w-admin/core/utils/response.py
@@ -3,7 +3,13 @@ from django.core.files import File
 
 
 def send_file(output_filename, content_type, file, attachment=True):
-
+    """
+    Send a Django HttpRensponse with attached file
+    :param output_filename: file name to send.
+    :param content_type: mime type file to send.
+    :param attachment: True default, to set Content-Disposition http header.
+    :return: Django HttpResponse instance.
+    """
     response = HttpResponse(File(open(file, 'rb')), content_type=content_type)
     if attachment:
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(output_filename)
