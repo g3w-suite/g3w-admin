723cce28f03349ad2ea35a8f61af5470e9f16846
wlorenzetti <lorenzetti@gis3w.it>
2020-04-30 08:02:20 +0200

    Add md tables support to sphinx doc system.

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx_rtd_theme',
-    'recommonmark'
+    'recommonmark',
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
