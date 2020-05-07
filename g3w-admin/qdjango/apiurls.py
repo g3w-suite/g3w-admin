04fb82c70747b6b5ac461879d62ec262ade27b96
wlorenzetti <lorenzetti@gis3w.it>
2020-05-07 16:00:07 +0200

    Remove qdjango-api-project-relations url name.

--- g3w-admin/qdjango/apiurls.py
+++ g3w-admin/qdjango/apiurls.py
@@ -11,14 +11,8 @@ from .api.constraints.views import (
     SingleLayerConstraintList,
 )
 
-urlpatterns = [
-    url(r'^api/relations/(?P<project_id>[0-9]+)/(?P<relation_id>[-_\w\d]+)/(?P<relation_field_value>[/*\-+,._\w\d]+)$',
-        QdjangoProjectRelationsApiView.as_view(),
-        name='qdjango-api-project-relations'),
-]
-
 # Single layer Constraints
-urlpatterns += [
+urlpatterns = [
 
     #############################################################
     # Subset string rules
