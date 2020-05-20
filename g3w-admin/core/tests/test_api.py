44cbc5c6f55e67f6ed55e8cd7d1c4e929e88ede3
Alessandro Pasotti <elpaso@itopen.it>
2020-05-20 08:37:00 +0200

    Test cleanup

--- g3w-admin/core/tests/test_api.py
+++ g3w-admin/core/tests/test_api.py
@@ -30,22 +30,6 @@ from .base import CoreTestBase
 DATASOURCE_PATH = os.path.join(os.getcwd(), 'qdjango', 'tests', 'data')
 
 
-def base_data_test(cls):
-
-    # Fill the cache with getprojectsettings response so we don't need a QGIS instance running
-    # TODO: eventually move to QgsServer
-    cls.prj = Project.objects.get(title='Un progetto')
-    cache_key = settings.QDJANGO_PRJ_CACHE_KEY.format(cls.prj.pk)
-    cache = caches['qdjango']
-    cache.set(cache_key, open(os.path.join(DATASOURCE_PATH,
-                                           'getProjectSettings_gruppo-1_un-progetto.xml')).read())
-
-    # Fix datasource path for spatialite
-    l = Layer.objects.get(name='spatialite_points')
-    l.datasource = 'dbname=\'%s/un-progetto-data/un-progetto.db\' table="spatialite_points" (geom) sql=' % DATASOURCE_PATH
-    l.save()
-
-
 @override_settings(MEDIA_ROOT=DATASOURCE_PATH)
 @override_settings(DATASOURCE_PATH=DATASOURCE_PATH)
 @override_settings(CACHES={
@@ -67,7 +51,6 @@ class CoreApiTest(CoreTestBase):
     @classmethod
     def setUpClass(cls):
         super(CoreApiTest, cls).setUpClass()
-        base_data_test(cls)
 
     def testCoreVectorApiShp(self):
         """Test core-vector-api shp"""
@@ -80,7 +63,7 @@ class CoreApiTest(CoreTestBase):
     def testCoreVectorApiConfig(self):
         """Test core-vector-api config"""
 
-        # Add a constraint expression to name field
+        # Add a constraint expression to "name" field
         project = get_qgs_project(Layer.objects.get(
             qgs_layer_id='spatialite_points20190604101052075').project.qgis_file.path)
         layer = project.mapLayers()['spatialite_points20190604101052075']
