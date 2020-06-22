1e401c7c796dc2ca02359f711b7aaecf0b805566
wlorenzetti <lorenzetti@gis3w.it>
2020-06-22 10:40:28 +0200

    Remove shapely dependence.

--- g3w-admin/core/api/serializers.py
+++ g3w-admin/core/api/serializers.py
@@ -6,8 +6,6 @@ from django.core.exceptions import ValidationError
 from guardian.shortcuts import get_objects_for_user, get_user_model
 from rest_framework import serializers
 from rest_framework.fields import empty
-from rest_framework_gis.fields import GeometryField
-from shapely import wkt, geometry
 from core.models import Group, BaseLayer
 from core.signals import initconfig_plugin_start
 from core.mixins.api.serializers import G3WRequestSerializer
@@ -201,25 +199,6 @@ class GroupSerializer(G3WRequestSerializer, serializers.ModelSerializer):
             'header_terms_of_use_text',
             'header_terms_of_use_link'
         )
-
-
-class G3WGeometryField(GeometryField):
-    """
-    A field to handle GeoDjango Geometry fields
-    """
-
-    def to_internal_value(self, value):
-        if value == '' or value is None:
-            return value
-        if isinstance(value, GEOSGeometry):
-            # value already has the correct representation
-            return value
-        if isinstance(value, dict):
-            value = wkt.dumps(geometry.shape(value))
-        try:
-            return GEOSGeometry(value)
-        except (ValueError, GEOSException, GDALException, TypeError):
-            raise ValidationError(_('Invalid format: string or unicode input unrecognized as GeoJSON, WKT EWKT or HEXEWKB.'))
 
 
 class G3WSerializerMixin(object):
--- g3w-admin/core/apps.py
+++ g3w-admin/core/apps.py
@@ -5,37 +5,3 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     name = 'core'
     verbose_name = 'G3W-Admin main app'
-
-    def ready(self):
-        """
-        update Django Rest Framework serializer mappings
-        """
-
-        # import signal handlers
-        import core.receivers
-
-        from django.contrib.gis.db import models
-        from rest_framework.serializers import ModelSerializer
-        try:
-            from editing.api.serializers import G3WGeometryField as GeometryField
-
-            try:
-                # drf 3.0
-                field_mapping = ModelSerializer._field_mapping.mapping
-            except AttributeError:
-                # drf 3.1
-                field_mapping = ModelSerializer.serializer_field_mapping
-
-            # map GeoDjango fields to drf-gis GeometryField
-            field_mapping.update({
-                models.GeometryField: GeometryField,
-                models.PointField: GeometryField,
-                models.LineStringField: GeometryField,
-                models.PolygonField: GeometryField,
-                models.MultiPointField: GeometryField,
-                models.MultiLineStringField: GeometryField,
-                models.MultiPolygonField: GeometryField,
-                models.GeometryCollectionField: GeometryField
-            })
-        except Exception:
-            pass
