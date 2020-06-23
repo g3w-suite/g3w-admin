d69bc6d4ae85acf277890d19927f38841a869f53
wlorenzetti <lorenzetti@gis3w.it>
2020-06-23 14:56:19 +0200

    Add download_gpx field.

--- g3w-admin/qdjango/models/projects.py
+++ g3w-admin/qdjango/models/projects.py
@@ -368,6 +368,8 @@ class Layer(G3WACLModelMixins, models.Model):
         _('Download data'), default=False, blank=True)
     download_xls = models.BooleanField(
         _('Download data in xls format'), default=False, blank=True)
+    download_gpx = models.BooleanField(
+        _('Download data in gpx format'), default=False, blank=True)
 
     # for layer WMS/WMST: set if load direct from their servers or from local QGIS-server
     external = models.BooleanField(
