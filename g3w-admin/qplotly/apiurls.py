c1e09a41236ed27fb5ad8a59cf3520c9be82a9ff
wlorenzetti <lorenzetti@gis3w.it>
2020-09-16 14:52:04 +0200

    Add empty apiurls module.

---
+++ g3w-admin/qplotly/apiurls.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+""""qplotly api ulrs
+
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2020-09-16'
+__copyright__ = 'Copyright 2015 - 2020, Gis3w'
+
+
+urlpatterns = []
