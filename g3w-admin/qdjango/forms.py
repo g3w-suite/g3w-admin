28c2fd264039c725f14d3b44b9477541180a0e6b
wlorenzetti <lorenzetti@gis3w.it>
2020-12-30 09:05:37 +0100

    Restore context_base_legend policy to QGIS desktop.

--- g3w-admin/qdjango/forms.py
+++ g3w-admin/qdjango/forms.py
@@ -170,7 +170,6 @@ class QdjangoProjetForm(TranslationModelForm, QdjangoProjectFormMixin, G3WFormMi
                                         ),
                                         Div(
                                             'use_map_extent_as_init_extent',
-                                            'context_base_legend',
                                             'feature_count_wms',
                                             'multilayer_query',
                                             'multilayer_querybybbox',
@@ -199,7 +198,6 @@ class QdjangoProjetForm(TranslationModelForm, QdjangoProjectFormMixin, G3WFormMi
             'multilayer_querybybbox',
             'multilayer_querybypolygon',
             'use_map_extent_as_init_extent',
-            'context_base_legend',
             'title_ur'
         )
 
