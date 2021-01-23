52022de65d39264629d0b1552d0c9935ec895c5c
wlorenzetti <lorenzetti@gis3w.it>
2021-01-23 11:57:29 +0100

    Proting changes for catalog from dev to v3.1.x.

--- g3w-admin/core/mixins/api/serializers.py
+++ g3w-admin/core/mixins/api/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework.fields import empty
 
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 class G3WRequestSerializer(object):
     """
@@ -7,7 +10,11 @@ class G3WRequestSerializer(object):
     """
     def __init__(self, instance=None, data=empty, **kwargs):
 
-        self.request = kwargs['request']
-        del (kwargs['request'])
+        try:
+            self.request = kwargs['request']
+            del (kwargs['request'])
+        except:
+            LOGGER.warning('Serializer without request: it might be ok when called from a management command')
+            self.request = None
 
         super(G3WRequestSerializer, self).__init__(instance, data, **kwargs)
--- g3w-admin/qdjango/utils/models.py
+++ g3w-admin/qdjango/utils/models.py
@@ -61,3 +61,27 @@ def get_constraints4layer(layer):
     """
 
     return layer.constrainted_layer.all()
+
+
+class temp_disconnect_signal():
+    """ Temporarily disconnect a model from a signal """
+
+    def __init__(self, signal, receiver, sender, dispatch_uid=None):
+        self.signal = signal
+        self.receiver = receiver
+        self.sender = sender
+        self.dispatch_uid = dispatch_uid
+
+    def __enter__(self):
+        self.signal.disconnect(
+            receiver=self.receiver,
+            sender=self.sender,
+            dispatch_uid=self.dispatch_uid
+        )
+
+    def __exit__(self, type, value, traceback):
+        self.signal.connect(
+            receiver=self.receiver,
+            sender=self.sender,
+            dispatch_uid=self.dispatch_uid
+        )
