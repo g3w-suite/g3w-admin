41f82b00552f98b1fa6aa217bde25c674476413a
wlorenzetti <lorenzetti@gis3w.it>
2021-02-19 11:41:02 +0100

    Fix issue #206: first selectbox widget search values list sorted.

--- g3w-admin/qdjango/api/projects/serializers.py
+++ g3w-admin/qdjango/api/projects/serializers.py
@@ -606,6 +606,9 @@ class WidgetSerializer(serializers.ModelSerializer):
                             logger.error(f'Response vector widget unique: {e}')
                             continue
 
+                    # sort values for selectbox
+                    values.sort()
+
                     field['input']['type'] = 'selectfield'
                     if 'dependance' not in field['input']['options']:
 
