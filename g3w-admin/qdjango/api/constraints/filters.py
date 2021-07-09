6ebfb81008abd00124dd9344830dd4b4d1e172ed
wlorenzetti <lorenzetti@gis3w.it>
2021-07-09 14:02:10 +0200

    Add test for simglelayerconstraint for anonymoususer.

--- g3w-admin/qdjango/api/constraints/filters.py
+++ g3w-admin/qdjango/api/constraints/filters.py
@@ -78,9 +78,9 @@ class GeoConstraintsFilter(BaseFilterBackend):
 
         if rule_parts:
             expression = ' AND '.join(rule_parts)
-            current_expression = qgis_feature_request.filterExpression().expression()
+            current_expression = qgis_feature_request.filterExpression()
 
             if current_expression:
-                expression = f'({current_expression}) AND ({expression})'
+                expression = f'({current_expression.expression()}) AND ({expression})'
 
             qgis_feature_request.setFilterExpression(expression)
