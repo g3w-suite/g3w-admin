4139495e2f3300fff56cfba91477663a0398e92d
wlorenzetti <lorenzetti@gis3w.it>
2021-08-31 09:36:30 +0200

    Add 'MultiPoint' to GPX export geometry type to exclude.

--- g3w-admin/qdjango/templatetags/qdjango_tags.py
+++ g3w-admin/qdjango/templatetags/qdjango_tags.py
@@ -34,7 +34,7 @@ def is_geom_type_gpx_compatible(layer):
     Return True if layer type can be exported as GPS format.
     """
 
-    return layer.geometrytype not in ['Polygon', 'MultiPolygon', 'NoGeometry']
+    return layer.geometrytype not in ['Polygon', 'MultiPolygon', 'NoGeometry', 'MultiPoint']
 
 
 @register.simple_tag()
