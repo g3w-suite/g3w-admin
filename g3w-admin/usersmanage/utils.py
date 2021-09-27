bb21f1443ddd4479d11b90eefecd16701e92555c
wlorenzetti <lorenzetti@gis3w.it>
2021-09-27 18:06:03 +0200

    Doc typo.

--- g3w-admin/usersmanage/utils.py
+++ g3w-admin/usersmanage/utils.py
@@ -123,7 +123,7 @@ def get_fields_by_user(user, form, **kwargs):
 def get_users_for_object(object, permission, group=None, with_anonymous = False, with_group_users=False):
     """
     Returns list of users(worn:not QuerySet) with specific permission for this object
-    :param obejct: model object to check permission
+    :param object: model object to check permission
     :param permission: permission string
     :param group: group name for filter
     :param with_anonimous: add anonimous user to return value if it has permission on object, if group is set
