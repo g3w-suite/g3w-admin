e7148a3f3030114b9eba72df2833cd927363be43
wlorenzetti <lorenzetti@gis3w.it>
2021-11-24 08:30:09 +0100

    Continue develo main workflow to create project with raster time series features.

--- g3w-admin/qrastertimeseries/models.py
+++ g3w-admin/qrastertimeseries/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+# coding=utf-8
+""""Module with models for qrastertimeseries.
 
-# Create your models here.
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2021-11-23'
+__copyright__ = 'Copyright 2015 - 2021, Gis3w'
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from qdjango.models import Project, Layer
+
+
+class QRasterTimeSeriesProject(models.Model):
+    """ Main QRasterTimeSeries projects """
+
+    project = models.OneToOneField(Project, on_delete=models.CASCADE, related_name="%(app_label)s_projects")
+    note = models.TextField('Note', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'QRasterTimeSeries Project'
+
+    def __str__(self):
+        return str(self.project)
+
+
+class QRasterTimeSeriesLayer(models.Model):
+    """ For every QRasterTimeSeries projects vector layer to use """
+
+    qrastertimeseries_project = models.OneToOneField(QRasterTimeSeriesProject, on_delete=models.CASCADE,
+                                              related_name="qrastertimeseries_project", null=True)
+    layer = models.ForeignKey(Layer, on_delete=models.CASCADE, related_name='qrastertimeseries_layer', null=True,
+                                  help_text=_('Select NETCDF raster project layer to use'))
+    note = models.TextField('Note', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'QRasterTimeSeries vector Layer'
+
+    def __str__(self):
+        return f"{self.qrastertimeseries_project} - {self.layer}"
--- g3w-admin/qrastertimeseries/views.py
+++ g3w-admin/qrastertimeseries/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+# coding=utf-8
+""""Module with views for qrastertimeseries.
 
-# Create your views here.
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2021-11-23'
+
+from django.views.generic import \
+    ListView, \
+    CreateView
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from guardian.decorators import permission_required
+from core.mixins.views import G3WRequestViewMixin, G3WAjaxDeleteViewMixin
+from .models import QRasterTimeSeriesProject
+from .forms import QRasterTimeSeriesProjectForm
+
+
+class QRasterTimeSeriesProjectsListView(ListView):
+    """List simple qrastertimeseries projects view."""
+    template_name = 'qrastertimeseries/projects_list.html'
+    model = QRasterTimeSeriesProject
+
+    @method_decorator(permission_required('qrastertimeseries.add_qrastertimeseriesproject', return_403=True))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+
+class QRasterTimeSeriesProjectsAddView(G3WRequestViewMixin, CreateView):
+    """
+    Create view for single qrastertimeseries project
+    """
+    form_class = QRasterTimeSeriesProjectForm
+    template_name = 'qrastertimeseries/project_form.html'
+    success_url = reverse_lazy('qrastertimeseries-project-list')
+
+    @method_decorator(permission_required('qrastertimeseries.add_qrastertimeseriesproject', return_403=True))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
