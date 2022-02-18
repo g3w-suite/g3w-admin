a5ec37162272675954ac35ec0da26beac8a442eb
Alessandro Pasotti <elpaso@itopen.it>
2022-02-18 16:16:49 +0100

    Move expression logic to core API

--- g3w-admin/core/api/views.py
+++ g3w-admin/core/api/views.py
@@ -4,9 +4,60 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from base.version import get_version
 from .base.views import G3WAPIView
+from core.api.authentication import CsrfExemptSessionAuthentication
+from core.utils.qgisapi import expression_eval
+from qdjango.models import Project
+from core.api.base.views import APIException
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import status
+
+from qgis.core import (
+    QgsExpression,
+    QgsExpressionContext,
+)
+
 import platform
 
-from qgis.core import Qgis
+from qgis.core import (
+    Qgis,
+)
+
+from core.utils.qgisapi import (
+    expression_eval,
+    ExpressionEvalError,
+    ExpressionFormDataError,
+    ExpressionParseError,
+    ExpressionLayerError
+)
+
+
+class APIExpressionEvalError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Expression evaluation error.')
+    default_code = 'expression_eval_error'
+
+class APIExpressionParseError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Expression parse error.')
+    default_code = 'expression_parse_error'
+
+
+class APIExpressionEmptyError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Expression empty error.')
+    default_code = 'expression_empty_error'
+
+
+class APIExpressionFormDataError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Expression form data error.')
+    default_code = 'expression_form_data_error'
+
+
+class APIExpressionLayerError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Layer error.')
+    default_code = 'expression_layer_error'
 
 
 LAYERVECTORVIEW_CLASS_DEFAULT = 'LayerVectorView'
@@ -14,13 +65,16 @@ LAYERVECTORVIEW_CLASSES = dict()
 USERMEDIAHANDLER_CLASSES = dict()
 
 for app_name in settings.G3WADMIN_PROJECT_APPS:
-    #try:
+    # try:
     projectAppModule = __import__('{}.vector'.format(app_name))
-    LAYERVECTORVIEW_CLASSES[app_name] = getattr(projectAppModule.vector, LAYERVECTORVIEW_CLASS_DEFAULT)
-    USERMEDIAHANDLER_CLASSES[app_name] = getattr(projectAppModule.vector, 'UserMediaHandler')
-    #except Exception as e:
+    LAYERVECTORVIEW_CLASSES[app_name] = getattr(
+        projectAppModule.vector, LAYERVECTORVIEW_CLASS_DEFAULT)
+    USERMEDIAHANDLER_CLASSES[app_name] = getattr(
+        projectAppModule.vector, 'UserMediaHandler')
+    # except Exception as e:
     #    print(e)
     #    continue
+
 
 @csrf_exempt  # put exempt here because as_view method is outside url bootstrap declaration
 def layer_vector_view(request, project_type, project_id, layer_name, *args, **kwargs):
@@ -28,7 +82,8 @@ def layer_vector_view(request, project_type, project_id, layer_name, *args, **kw
 
     # instance module vector view
     view = LAYERVECTORVIEW_CLASSES[project_type].as_view()
-    kwargs.update({'project_type': project_type, 'project_id': project_id, 'layer_name': layer_name})
+    kwargs.update({'project_type': project_type,
+                   'project_id': project_id, 'layer_name': layer_name})
     return view(request, *args, **kwargs)
 
 
@@ -60,3 +115,59 @@ class G3WSUITEInfoAPIView(G3WAPIView):
         return Response(self.results.results)
 
 
+class QgsExpressionLayerContextEvalView(G3WAPIView):
+    """This POST only API accepts a QgsExpression, a qdjango project_id and optionally a
+        QGIS layer id and a GeoJSON feature data and returns the evaluated QgsExpression
+        in the feature form context.
+
+    Mandatory payload:
+
+    `qexpression`: text of the QgsExpression to be evaluated,
+
+    Optional JSON payload:
+
+    `form_data`: GeoJSON representation of the feature currently being edited.
+    `qgs_layer_id`: QGIS layer id
+
+    """
+
+    authentication_classes = (
+        CsrfExemptSessionAuthentication,
+    )
+
+    def post(self, request, project_id, format=None):
+        """
+        This method accepts a QgsExpression a qdjango project_id and  QGIS layer ID and an optional geojson
+        feature data and returns the evaluated QgsExpression in the feature form context.
+        """
+
+        if request.content_type != 'application/json':
+            try:
+                data = json.loads(request.data.get('_content'))
+                expression_text = data.get('expression')
+                form_data = data.get('form_data')
+                qgs_layer_id = data.get('qgs_layer_id')
+            except:
+                raise APIExpressionEmptyError()
+        else:
+            expression_text = request.data.get('expression')
+            form_data = request.data.get('form_data')
+            form_data = request.data.get('form_data')
+            qgs_layer_id = request.data.get('qgs_layer_id')
+
+        if expression_text is None:
+            raise APIExpressionEmptyError()
+
+        try:
+            result = expression_eval(
+                expression_text, project_id, qgs_layer_id, form_data)
+        except ExpressionFormDataError as ex:
+            raise APIExpressionFormDataError(str(ex))
+        except ExpressionEvalError as ex:
+            raise APIExpressionEvalError(str(ex))
+        except ExpressionParseError as ex:
+            raise APIExpressionParseError(str(ex))
+        except ExpressionLayerError as ex:
+            raise APIExpressionLayerError(str(ex))
+
+        return Response(result)
--- g3w-admin/core/tests/test_qgisapi.py
+++ g3w-admin/core/tests/test_qgisapi.py
@@ -20,16 +20,30 @@ from django.contrib.auth.models import User
 from qdjango.models import Project
 from django.core.cache import caches
 from qdjango.models import Layer
+from .base import CoreTestBase
 
-from core.utils.qgisapi import get_qgis_layer, get_qgis_features
-from qgis.core import QgsRectangle
+
+from core.utils.qgisapi import (
+    get_qgis_layer,
+    get_qgis_features,
+    expression_eval,
+    ExpressionEvalError,
+    ExpressionForbiddenError,
+    ExpressionFormDataError,
+    ExpressionLayerError,
+    ExpressionProjectError,
+    ExpressionParseError,
+
+)
+from qgis.core import QgsRectangle, QgsJsonExporter
 
 # Re-use test data from qdjango module
 DATASOURCE_PATH = os.path.join(os.getcwd(), 'qdjango', 'tests', 'data')
 
+
 @override_settings(MEDIA_ROOT=DATASOURCE_PATH)
 @override_settings(DATASOURCE_PATH=DATASOURCE_PATH)
-@override_settings(CACHES = {
+@override_settings(CACHES={
     'qdjango': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         'LOCATION': 'some',
@@ -54,14 +68,16 @@ class CoreQgisApiTest(APITestCase):
         try:
             cls.user = User.objects.get(username='admin%s' % cls.__class__)
         except:
-            cls.user = User.objects.create_superuser(username='admin%s' % cls.__class__, password='admin', email='')
+            cls.user = User.objects.create_superuser(
+                username='admin%s' % cls.__class__, password='admin', email='')
 
         # Fill the cache with getprojectsettings response so we don't need a QGIS instance running
         # TODO: eventually move to QgsServer
         prj = Project.objects.get(title='Un progetto')
         cache_key = settings.QDJANGO_PRJ_CACHE_KEY.format(prj.pk)
         cache = caches['qdjango']
-        cache.set(cache_key, open(os.path.join(DATASOURCE_PATH, 'getProjectSettings_gruppo-1_un-progetto.xml')).read())
+        cache.set(cache_key, open(os.path.join(DATASOURCE_PATH,
+                                               'getProjectSettings_gruppo-1_un-progetto.xml')).read())
 
         # Fix datasource path for spatialite
         l = Layer.objects.get(name='spatialite_points')
@@ -232,7 +248,8 @@ class CoreQgisApiTest(APITestCase):
         qgis_layer = get_qgis_layer(self.layer)
         self.assertTrue(qgis_layer.isValid())
 
-        features = get_qgis_features(qgis_layer, bbox_filter=qgis_layer.extent())
+        features = get_qgis_features(
+            qgis_layer, bbox_filter=qgis_layer.extent())
         self.assertEqual(len(features), 2)
 
         bbox = QgsRectangle(
@@ -269,7 +286,8 @@ class CoreQgisApiTest(APITestCase):
         features = get_qgis_features(qgis_layer)
         self.assertEqual(len(features), 2)
 
-        features = get_qgis_features(qgis_layer, extra_subset_string='name != \'another point\'')
+        features = get_qgis_features(
+            qgis_layer, extra_subset_string='name != \'another point\'')
         self.assertEqual(len(features), 1)
         self.assertEqual(features[0]['name'], 'a point')
 
@@ -277,7 +295,8 @@ class CoreQgisApiTest(APITestCase):
         features = get_qgis_features(qgis_layer)
         self.assertEqual(len(features), 2)
 
-        features = get_qgis_features(qgis_layer, extra_subset_string='name == \'another point\'')
+        features = get_qgis_features(
+            qgis_layer, extra_subset_string='name == \'another point\'')
         self.assertEqual(len(features), 1)
         self.assertEqual(features[0]['name'], 'another point')
 
@@ -288,7 +307,8 @@ class CoreQgisApiTest(APITestCase):
         self.assertEqual(len(features), 1)
         self.assertEqual(features[0]['name'], 'another point')
 
-        features = get_qgis_features(qgis_layer_clone, extra_subset_string='name != \'another point\'')
+        features = get_qgis_features(
+            qgis_layer_clone, extra_subset_string='name != \'another point\'')
         self.assertEqual(len(features), 0)
 
         # Check if restored
@@ -296,3 +316,121 @@ class CoreQgisApiTest(APITestCase):
         features = get_qgis_features(qgis_layer_clone)
         self.assertEqual(len(features), 1)
         self.assertEqual(features[0]['name'], 'another point')
+
+    def test_expression_eval(self):
+
+        self.assertEqual(expression_eval('1'), 1)
+        self.assertEqual(expression_eval('1=2'), False)
+
+        layer = Layer.objects.get(title='world')
+
+        # Errors
+        with self.assertRaises(ExpressionProjectError) as ex:
+            expression_eval('1', project_id='9999', qgs_layer_id='9999')
+
+
+        with self.assertRaises(ExpressionLayerError) as ex:
+            expression_eval('1', project_id=layer.project_id,
+                            qgs_layer_id='9999')
+
+        with self.assertRaises(ExpressionParseError) as ex:
+            expression_eval('dsa hdshk == t')
+
+        with self.assertRaises(ExpressionEvalError) as ex:
+            expression_eval('not_valid=2')
+
+        with self.assertRaises(ExpressionForbiddenError) as ex:
+            expression_eval('@qgis_version')
+
+        with self.assertRaises(ExpressionForbiddenError) as ex:
+            expression_eval('env(\'USER\')')
+
+        # Test form data
+        feature = next(layer.qgis_layer.getFeatures())
+        exp = QgsJsonExporter(layer.qgis_layer)
+        form_data = exp.exportFeature(feature)
+
+        self.assertEqual(expression_eval('current_value(\'APPROX\')', project_id=layer.project_id,
+                                         qgs_layer_id=layer.qgs_layer_id, form_data=json.loads(form_data)), 9705000)
+
+        self.assertEqual(expression_eval('"APPROX"', project_id=layer.project_id,
+                                         qgs_layer_id=layer.qgs_layer_id, form_data=json.loads(form_data)), 9705000)
+
+        self.assertEqual(expression_eval('APPROX', project_id=layer.project_id,
+                                         qgs_layer_id=layer.qgs_layer_id, form_data=json.loads(form_data)), 9705000)
+
+        self.assertEqual(expression_eval('APPROX = 9705000', project_id=layer.project_id,
+                                         qgs_layer_id=layer.qgs_layer_id, form_data=json.loads(form_data)), True)
+
+        self.assertEqual(expression_eval('APPROX = 99999', project_id=layer.project_id,
+                                         qgs_layer_id=layer.qgs_layer_id, form_data=json.loads(form_data)), False)
+
+
+@override_settings(MEDIA_ROOT=DATASOURCE_PATH)
+@override_settings(DATASOURCE_PATH=DATASOURCE_PATH)
+class LayerExpressionEval(CoreTestBase):
+    """ Test for QgsExpression evaluation API"""
+
+    # These are stored in core module
+    fixtures = CoreTestBase.fixtures + [
+        # except for this one which is in qdjango:
+        "G3WSampleProjectAndGroup.json",
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = APIClient()
+
+    def _expression_evaluate(self, url, expression, expected, form_data=None, qgs_layer_id=None):
+
+        assert self.client.login(
+            username=self.test_admin1.username, password=self.test_admin1.username)
+
+        data = {
+            'expression': expression
+        }
+
+        if form_data is not None:
+            data['form_data'] = form_data
+            data['qgs_layer_id'] = qgs_layer_id
+
+        response = self.client.post(
+            url, data, format='json', content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        jcontent = json.loads(response.content)
+        self.assertEqual(jcontent, expected)
+
+    def test_layer_expression_eval(self):
+
+        project = Project.objects.all()[0]
+        world = project.layer_set.get(title='world')
+        points = project.layer_set.get(title='spatialite_points')
+
+        url = reverse('layer-expression-eval',
+                      args=[project.pk])
+
+        self._expression_evaluate(url, '1', 1)
+
+        form_data = {
+            'bbox': [-92.28845, 13.7392, -88.23725, 17.783133],
+            'geometry': {
+                'coordinates': [[[[
+                    -92.22255, 14.519233],
+                    [-90.0424, 13.887533],
+                    [-91.6125, 14.1068],
+                    [-91.6343, 14.121333],
+                    [-91.64955, 14.121333],
+                    [-91.667, 14.114067],
+                    [-91.68875, 14.114067],
+                    [-91.81295, 14.215733],
+                    [-91.9524, 14.327567],
+                    [-92.22255, 14.519233]
+                ]]],
+                'type': 'MultiPolygon'}, 'id': 0,
+            'properties': {'APPROX': 9705000, 'AREA': 109727.89624587, 'CAPITAL': 'GUATEMALA', 'NAME': 'GUATEMALA', 'SOURCETHM': 'Country_col_regi'},
+            'type': 'Feature'}
+
+        self._expression_evaluate(
+            url, 'current_value(\'CAPITAL\')', "GUATEMALA", form_data, world.qgs_layer_id)
