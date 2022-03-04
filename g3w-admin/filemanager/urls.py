050c8c3837ff53d97d148d31833208393c0ec8fa
wlorenzetti <lorenzetti@gis3w.it>
2022-03-04 15:28:18 +0100

    Refactoring: migration from url() to path().

--- g3w-admin/filemanager/urls.py
+++ g3w-admin/filemanager/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.contrib.auth.decorators import login_required
 from base.urls import G3W_SITETREE_I18N_ALIAS
 from .views import *
@@ -7,7 +7,7 @@ from .views import *
 G3W_SITETREE_I18N_ALIAS.append('filemanager_sidebar_right')
 
 urlpatterns = [
-    url(r'^$', login_required(FilemanagerView.as_view()), name='filemanager-home'),
-    url(r'^config/(?P<file_js>[-_\.\\\w\d]+)$', login_required(FilemanagerServeConfigView.as_view()),
+    path('', login_required(FilemanagerView.as_view()), name='filemanager-home'),
+    re_path(r'^config/(?P<file_js>[-_\.\\\w\d]+)$', login_required(FilemanagerServeConfigView.as_view()),
        name='filemanager-serve-file-config'),
 ]
