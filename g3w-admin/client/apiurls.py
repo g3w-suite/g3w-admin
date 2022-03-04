3d2cd780bbc38853603abdde8bf527f6b2a98d11
wlorenzetti <lorenzetti@gis3w.it>
2022-03-04 12:15:19 +0100

    refactoring

--- g3w-admin/client/apiurls.py
+++ g3w-admin/client/apiurls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .api.views import *
-from .views import ClientView
+from .api.views import \
+    GroupConfigApiView, \
+    ClientConfigApiView
 
 urlpatterns = [
 
-    #Main init client API rest initialization
-    path('api/initconfig/<slug:group_slug>/<project_type>/<int:project_id>',
-        GroupConfigApiView.as_view(), name='group-map-config'),
-    path('api/config/<slug:group_slug>/<project_type>/<int:project_id>',
-        ClientConfigApiView.as_view(), name='group-project-map-config'),
+    # Main init client API rest initialization
+    # ========================================
+    path('api/initconfig/<slug:group_slug>/<project_type>/<int:project_id>', GroupConfigApiView.as_view(),
+         name='group-map-config'),
+
+    path('api/config/<slug:group_slug>/<project_type>/<int:project_id>', ClientConfigApiView.as_view(),
+         name='group-project-map-config'),
 ]
