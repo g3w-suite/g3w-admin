9411efce7516bfe9cb226518ab467491f67d56a7
wlorenzetti <lorenzetti@gis3w.it>
2022-03-04 15:39:24 +0100

    Refactoring: migration from url() to path().

--- g3w-admin/usersmanage/urls.py
+++ g3w-admin/usersmanage/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 from usersmanage.views import *
 
@@ -6,23 +6,23 @@ from usersmanage.views import *
 urlpatterns = [
 
     # user groups managment
-    url(r'^users/groups/$', login_required(UserGroupListView.as_view()), name='user-group-list'),
-    url(r'^users/groups/add/$', login_required(UserGroupCreateView.as_view()), name='user-group-add'),
-    url(r'^users/groups/(?P<pk>[-_\w\d]+)/$',login_required(UserGroupDetailView.as_view()), name='user-group-detail'),
-    url(r'^users/groups/update/(?P<pk>[-_\w\d]+)/$',login_required(UserGroupUpdateView.as_view()),
+    path('users/groups/', login_required(UserGroupListView.as_view()), name='user-group-list'),
+    path('users/groups/add/', login_required(UserGroupCreateView.as_view()), name='user-group-add'),
+    path('users/groups/<int:pk>/', login_required(UserGroupDetailView.as_view()), name='user-group-detail'),
+    path('users/groups/update/<int:pk>/', login_required(UserGroupUpdateView.as_view()),
         name='user-group-update'),
-    url(r'^users/groups/delete/(?P<pk>[-_\w\d]+)/$',login_required(UserGroupAjaxDeleteView.as_view()),
+    path('users/groups/delete/<int:pk>/', login_required(UserGroupAjaxDeleteView.as_view()),
         name='user-group-delete'),
 
-    url(r'^jx/user/groups/byuserrole/$', login_required(UserGroupByUserRoleView.as_view()),
+    path('jx/user/groups/byuserrole/', login_required(UserGroupByUserRoleView.as_view()),
         name='user-group-by-user-role'),
 
     # user management
-    url(r'^users/$', login_required(UserListView.as_view()), name='user-list'),
-    url(r'^users/add/$', login_required(UserCreateView.as_view()), name='user-add'),
-    url(r'^users/(?P<pk>[-_\w\d]+)/$',login_required(UserDetailView.as_view()), name='user-detail'),
-    url(r'^users/update/(?P<pk>[-_\w\d]+)/$',login_required(UserUpdateView.as_view()), name='user-update'),
-    url(r'^users/delete/(?P<pk>[-_\w\d]+)/$',login_required(UserAjaxDeleteView.as_view()), name='user-delete'),
+    path('users/', login_required(UserListView.as_view()), name='user-list'),
+    path('users/add/', login_required(UserCreateView.as_view()), name='user-add'),
+    path('users/<int:pk>/', login_required(UserDetailView.as_view()), name='user-detail'),
+    path('users/update/<int:pk>/', login_required(UserUpdateView.as_view()), name='user-update'),
+    path('users/delete/<int:pk>/', login_required(UserAjaxDeleteView.as_view()), name='user-delete'),
 
 
 
