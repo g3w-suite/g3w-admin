55d6711900ae36ee3b94c325900d50d4cdd9d931
wlorenzetti <lorenzetti@gis3w.it>
2022-03-04 15:08:28 +0100

    Refactoring: migration from url() to path().

--- g3w-admin/client/urls.py
+++ g3w-admin/client/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path, re_path
 from .views import *
 
 USER_MEDIA_PREFIX = 'me'
@@ -6,17 +7,17 @@ USER_MEDIA_PREFIX = 'me'
 urlpatterns = [
 
     # g3w-client bootstrap
-    url(r'^map/(?P<map_name_alias>[-_\w\d]+)/$', client_map_alias_view,
+    re_path(r'^map/(?P<map_name_alias>[-_\w\d]+)/$', client_map_alias_view,
         name='group-project-map-alias'),
-    url(r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_id>[0-9]+)/$', ClientView.as_view(),
+    re_path(r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_id>[0-9]+)/$', ClientView.as_view(),
         name='group-project-map'),
-    url(r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/$',
+    re_path(r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/$',
         ClientView.as_view(), name='group-project-slug-map'),
 
     # url for media reading upload
-    url(r'^{}/(?P<project_type>[-_\w\d]+)/(?P<layer_id>[0-9]+)/(?P<file_name>[-_. \w\d]+)'
+    re_path(r'^{}/(?P<project_type>[-_\w\d]+)/(?P<layer_id>[0-9]+)/(?P<file_name>[-_. \w\d]+)'
         .format(USER_MEDIA_PREFIX),
         user_media_view, name='user-media'),
 
-    url(r'^credits/$', credits, name='client-credits'),
+    path('credits/', credits, name='client-credits'),
 ]
