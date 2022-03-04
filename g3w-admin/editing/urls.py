ab723cb97e80cae083ca1d5c445e027650fcfcd8
wlorenzetti <lorenzetti@gis3w.it>
2022-03-04 15:26:58 +0100

    Refactoring: migration from url() to path().

--- g3w-admin/editing/urls.py
+++ g3w-admin/editing/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.contrib.auth.decorators import login_required
 from .views import UploadFileView, ActiveEditingLayerView
 
 urlpatterns = [
-    url(r'^upload/$', UploadFileView.as_view(), name='editing-upload'),
-    url(r'^(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/(?P<layer_id>[0-9]+)/'
+    path('upload/', UploadFileView.as_view(), name='editing-upload'),
+    re_path('^(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/(?P<layer_id>[0-9]+)/'
         r'active/$',
         login_required(ActiveEditingLayerView.as_view()), name='editing-layer-active'),
 ]
