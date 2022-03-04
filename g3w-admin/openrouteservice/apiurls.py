97610f9ef1c6b411bc10bd04f411b372b39ec731
wlorenzetti <lorenzetti@gis3w.it>
2022-03-04 12:31:57 +0100

    Refactoring: migration from url() to nw path() function.

--- g3w-admin/openrouteservice/apiurls.py
+++ g3w-admin/openrouteservice/apiurls.py
@@ -11,7 +11,7 @@ __date__ = '2021-03-09'
 __copyright__ = 'Copyright 2021, ItOpen'
 
 
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib.auth.decorators import login_required
 
 from .api.views import (
@@ -25,12 +25,12 @@ from .api.views import (
 BASE_URLS = 'openrouteservice'
 
 urlpatterns = [
-    url(r'^api/compatible_layers/(?P<project_id>[0-9]+)/$', login_required(
+    re_path(r'^api/compatible_layers/(?P<project_id>[0-9]+)/$', login_required(
         OpenrouteserviceCompatibleLayersView.as_view()), name='openrouteservice-compatible-layers'),
-    url(r'^api/isochrone/(?P<project_id>[0-9]+)/$', login_required(
+    re_path(r'^api/isochrone/(?P<project_id>[0-9]+)/$', login_required(
         OpenrouteServiceIsochroneView.as_view()), name='openrouteservice-isochrone'),
-    url(r'^api/isochrone_from_layer/(?P<project_id>[0-9]+)/(?P<layer_id>[0-9]+)$', login_required(
+    re_path(r'^api/isochrone_from_layer/(?P<project_id>[0-9]+)/(?P<layer_id>[0-9]+)$', login_required(
         OpenrouteServiceIsochroneFromLayerView.as_view()), name='openrouteservice-isochrone-from-layer'),
-    url(r'^api/isochrone_from_layer_result/(?P<project_id>[0-9]+)/(?P<task_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})$', login_required(
+    re_path(r'^api/isochrone_from_layer_result/(?P<project_id>[0-9]+)/(?P<task_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})$', login_required(
         OpenrouteServiceIsochroneFromLayerResultView.as_view()), name='openrouteservice-isochrone-from-layer-result'),
 ]
