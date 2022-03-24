e1b6ba279054b103dba63b9cab418bef93c6072a
wlorenzetti <lorenzetti@gis3w.it>
2022-03-24 14:55:06 +0100

    Fix check feature locked on delete.

--- g3w-admin/editing/api/base/views.py
+++ g3w-admin/editing/api/base/views.py
@@ -381,19 +381,13 @@ class BaseEditingVectorOnModelApiView(BaseVectorApiView):
 
             fids = post_layer_data[EDITING_POST_DATA_DELETED]
 
-            for fid in fids:
-                # control feature locked
-                if not metadata_layer.lock.checkFeatureLocked(fid):
-                    raise Exception(self.no_more_lock_feature_msg.format(
-                        fid, metadata_layer.client_var))
-
             # get feature fids from server fids from client.
             fids = get_layer_fids_from_server_fids([str(id) for id in fids], qgis_layer)
 
             for feature_id in fids:
 
                 # control feature locked
-                if not metadata_layer.lock.checkFeatureLocked(feature_id):
+                if not metadata_layer.lock.checkFeatureLocked(str(feature_id)):
                     raise Exception(self.no_more_lock_feature_msg.format(
                         feature_id, metadata_layer.client_var))
 
