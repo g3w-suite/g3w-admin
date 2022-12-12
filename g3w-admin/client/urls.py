8c65409976ab338818b268fae07d5ebe719fc1b3
Raruto <Raruto@users.noreply.github.com>
2022-12-12 11:41:56 +0100

    code format

--- g3w-admin/client/urls.py
+++ g3w-admin/client/urls.py
@@ -7,17 +7,34 @@ USER_MEDIA_PREFIX = 'me'
 urlpatterns = [
 
     # g3w-client bootstrap
-    re_path(r'^map/(?P<map_name_alias>[-_\w\d]+)/$', client_map_alias_view,
-        name='group-project-map-alias'),
-    re_path(r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_id>[0-9]+)/$', ClientView.as_view(),
-        name='group-project-map'),
-    re_path(r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/$',
-        ClientView.as_view(), name='group-project-slug-map'),
+    re_path(
+        r'^map/(?P<map_name_alias>[-_\w\d]+)/$',
+        client_map_alias_view,
+        name='group-project-map-alias'
+    ),
+
+    re_path(
+        r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_id>[0-9]+)/$',
+        ClientView.as_view(),
+        name='group-project-map'
+    ),
+
+    re_path(
+        r'^map/(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/$',
+        ClientView.as_view(),
+        name='group-project-slug-map'
+    ),
 
     # url for media reading upload
-    re_path(r'^{}/(?P<project_type>[-_\w\d]+)/(?P<layer_id>[0-9]+)/(?P<file_name>[\(\)-_. \w\d]+)'
+    re_path(
+        r'^{}/(?P<project_type>[-_\w\d]+)/(?P<layer_id>[0-9]+)/(?P<file_name>[\(\)-_. \w\d]+)'
         .format(USER_MEDIA_PREFIX),
-        user_media_view, name='user-media'),
+        user_media_view, name='user-media'
+    ),
 
-    path('credits/', credits, name='client-credits'),
+    path(
+        'credits/',
+        credits,
+        name='client-credits'
+    ),
 ]
--- g3w-admin/openrouteservice/apiurls.py
+++ g3w-admin/openrouteservice/apiurls.py
@@ -25,12 +25,27 @@ from .api.views import (
 BASE_URLS = 'openrouteservice'
 
 urlpatterns = [
-    re_path(r'^api/compatible_layers/(?P<project_id>[0-9]+)/$', login_required(
-        OpenrouteserviceCompatibleLayersView.as_view()), name='openrouteservice-compatible-layers'),
-    re_path(r'^api/isochrone/(?P<project_id>[0-9]+)/$', login_required(
-        OpenrouteServiceIsochroneView.as_view()), name='openrouteservice-isochrone'),
-    re_path(r'^api/isochrone_from_layer/(?P<project_id>[0-9]+)/(?P<layer_id>[0-9]+)$', login_required(
-        OpenrouteServiceIsochroneFromLayerView.as_view()), name='openrouteservice-isochrone-from-layer'),
-    re_path(r'^api/isochrone_from_layer_result/(?P<project_id>[0-9]+)/(?P<task_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})$', login_required(
-        OpenrouteServiceIsochroneFromLayerResultView.as_view()), name='openrouteservice-isochrone-from-layer-result'),
+    re_path(
+        r'^api/compatible_layers/(?P<project_id>[0-9]+)/$',
+        login_required(OpenrouteserviceCompatibleLayersView.as_view()),
+        name='openrouteservice-compatible-layers'
+    ),
+
+    re_path(
+        r'^api/isochrone/(?P<project_id>[0-9]+)/$',
+        login_required(OpenrouteServiceIsochroneView.as_view()),
+        name='openrouteservice-isochrone'
+    ),
+
+    re_path(
+        r'^api/isochrone_from_layer/(?P<project_id>[0-9]+)/(?P<layer_id>[0-9]+)$',
+        login_required(OpenrouteServiceIsochroneFromLayerView.as_view()),
+        name='openrouteservice-isochrone-from-layer'
+    ),
+
+    re_path(
+        r'^api/isochrone_from_layer_result/(?P<project_id>[0-9]+)/(?P<task_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})$',
+        login_required(OpenrouteServiceIsochroneFromLayerResultView.as_view()),
+        name='openrouteservice-isochrone-from-layer-result'
+    ),
 ]
