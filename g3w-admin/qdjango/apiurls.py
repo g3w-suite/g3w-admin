4c4f7fa991e6b6b6fee2341171cc2ed01db6492f
Raruto <Raruto@users.noreply.github.com>
2022-12-12 16:16:33 +0100

    code style (qdjango urls)

--- g3w-admin/qdjango/apiurls.py
+++ g3w-admin/qdjango/apiurls.py
@@ -55,174 +55,291 @@ urlpatterns = [
     # Subset string rules
 
     # Detail of a subsetstringrule
-    path('api/subsetstringrule/detail/<int:pk>/',
-         login_required(ConstraintSubsetStringRuleDetail.as_view()), name='qdjango-subsetstringrule-api-detail'),
+    path(
+        'api/subsetstringrule/detail/<int:pk>/',
+        login_required(ConstraintSubsetStringRuleDetail.as_view()),
+        name='qdjango-subsetstringrule-api-detail'
+    ),
 
     # All subsetstringrule(s) filtered by layer qdjango layer pk
-    path('api/subsetstringrule/layer/<int:layer_id>/', login_required(ConstraintSubsetStringRuleList.as_view()),
-         name='qdjango-subsetstringrule-api-filter-by-layer-id'),
+    path(
+        'api/subsetstringrule/layer/<int:layer_id>/',
+        login_required(ConstraintSubsetStringRuleList.as_view()),
+        name='qdjango-subsetstringrule-api-filter-by-layer-id'
+    ),
 
     # All subsetstringrule(s) filtered by User pk
-    path('api/subsetstringrule/user/<int:user_id>/', login_required(ConstraintSubsetStringRuleList.as_view()),
-         name='qdjango-subsetstringrule-api-filter-by-user'),
+    path(
+        'api/subsetstringrule/user/<int:user_id>/',
+        login_required(ConstraintSubsetStringRuleList.as_view()),
+        name='qdjango-subsetstringrule-api-filter-by-user'
+    ),
 
     # All subsetstringrule(s) filtered by Constraint pk
-    path('api/subsetstringrule/constraint/<int:constraint_id>/',
-         login_required(ConstraintSubsetStringRuleList.as_view()),
-         name='qdjango-subsetstringrule-api-filter-by-constraint'),
+    path(
+        'api/subsetstringrule/constraint/<int:constraint_id>/',
+        login_required(ConstraintSubsetStringRuleList.as_view()),
+        name='qdjango-subsetstringrule-api-filter-by-constraint'
+    ),
 
     # All subsetstringrule(s)
-    path('api/subsetstringrule/', login_required(ConstraintSubsetStringRuleList.as_view()),
-         name='qdjango-subsetstringrule-api-list'),
+    path(
+        'api/subsetstringrule/',
+        login_required(ConstraintSubsetStringRuleList.as_view()),
+        name='qdjango-subsetstringrule-api-list'
+    ),
 
     #############################################################
     # Expression rules
 
     # Detail of a expression rule
-    path('api/expressionrule/detail/<int:pk>/', login_required(ConstraintExpressionRuleDetail.as_view()),
-         name='qdjango-expressionrule-api-detail'),
+    path(
+        'api/expressionrule/detail/<int:pk>/',
+        login_required(ConstraintExpressionRuleDetail.as_view()),
+        name='qdjango-expressionrule-api-detail'
+    ),
 
     # All expressionrule(s) filtered by layer qdjango layer pk
-    path('api/expressionrule/layer/<int:layer_id>/', login_required(ConstraintExpressionRuleList.as_view()),
-         name='qdjango-expressionrule-api-filter-by-layer-id'),
+    path(
+        'api/expressionrule/layer/<int:layer_id>/',
+        login_required(ConstraintExpressionRuleList.as_view()),
+        name='qdjango-expressionrule-api-filter-by-layer-id'
+    ),
 
     # All expressionrule(s) filtered by User pk
-    path('api/expressionrule/user/<int:user_id>/', login_required(ConstraintExpressionRuleList.as_view()),
-         name='qdjango-expressionrule-api-filter-by-user'),
+    path(
+        'api/expressionrule/user/<int:user_id>/',
+        login_required(ConstraintExpressionRuleList.as_view()),
+        name='qdjango-expressionrule-api-filter-by-user'
+    ),
 
     # All expressionrule(s) filtered by Constraint pk
-    path('api/expressionrule/constraint/<int:constraint_id>/', login_required(ConstraintExpressionRuleList.as_view()),
-         name='qdjango-expressionrule-api-filter-by-constraint'),
+    path(
+        'api/expressionrule/constraint/<int:constraint_id>/',
+        login_required(ConstraintExpressionRuleList.as_view()),
+        name='qdjango-expressionrule-api-filter-by-constraint'
+    ),
 
     # All expressionrule(s)
-    path('api/expressionrule/', login_required(ConstraintExpressionRuleList.as_view()),
-         name='qdjango-expressionrule-api-list'),
+    path(
+        'api/expressionrule/',
+        login_required(ConstraintExpressionRuleList.as_view()),
+        name='qdjango-expressionrule-api-list'
+    ),
 
 
     #############################################################
     # Constraints
 
     # Detail of a Constraint
-    path('api/constraint/detail/<int:pk>/', login_required(SingleLayerConstraintDetail.as_view()),
-         name='qdjango-constraint-api-detail'),
+    path(
+        'api/constraint/detail/<int:pk>/',
+        login_required(SingleLayerConstraintDetail.as_view()),
+        name='qdjango-constraint-api-detail'
+    ),
 
     # All Constraint(s) filtered by layer qdjango layer pk
-    path('api/constraint/layer/<int:layer_id>/', login_required(SingleLayerConstraintList.as_view()),
-         name='qdjango-constraint-api-filter-by-layer-id'),
+    path(
+        'api/constraint/layer/<int:layer_id>/',
+        login_required(SingleLayerConstraintList.as_view()),
+        name='qdjango-constraint-api-filter-by-layer-id'
+    ),
 
     # All Constraint(s) filtered by user
-    path('api/constraint/user/<int:user_id>/', login_required(SingleLayerConstraintList.as_view()),
-         name='qdjango-constraint-api-filter-by-user'),
+    path(
+        'api/constraint/user/<int:user_id>/',
+        login_required(SingleLayerConstraintList.as_view()),
+        name='qdjango-constraint-api-filter-by-user'
+    ),
 
     # All Constraint(s)
-    path('api/constraint/', login_required(SingleLayerConstraintList.as_view()), name='qdjango-constraint-api-list'),
+    path(
+        'api/constraint/',
+        login_required(SingleLayerConstraintList.as_view()),
+        name='qdjango-constraint-api-list'
+    ),
 
     #############################################################
     # ColumnAcl
 
     # Detail of a ColumnAcl
-    path('api/column_acl/detail/<int:pk>/', login_required(ColumnAclDetail.as_view()),
-         name='qdjango-column-acl-api-detail'),
+    path(
+        'api/column_acl/detail/<int:pk>/',
+        login_required(ColumnAclDetail.as_view()),
+        name='qdjango-column-acl-api-detail'
+    ),
 
     # All (s) ColumnAcl filtered by layer qdjango layer pk
-    path('api/column_acl/layer/<int:layer_id>/',
-        login_required(ColumnAclList.as_view()), name='qdjango-column-acl-api-filter-by-layer-id'),
+    path(
+        'api/column_acl/layer/<int:layer_id>/',
+        login_required(ColumnAclList.as_view()),
+        name='qdjango-column-acl-api-filter-by-layer-id'
+    ),
 
     # All Constraint(s) filtered by user
-    path('api/column_acl/user/<int:user_id>/',
-        login_required(ColumnAclList.as_view()), name='qdjango-column-acl-api-filter-by-user'),
+    path(
+        'api/column_acl/user/<int:user_id>/',
+        login_required(ColumnAclList.as_view()),
+        name='qdjango-column-acl-api-filter-by-user'
+    ),
 
     # All Constraint(s) filtered by group
-    path('api/column_acl/group/<int:group_id>/',
-        login_required(ColumnAclList.as_view()), name='qdjango-column-acl-api-filter-by-group'),
+    path(
+        'api/column_acl/group/<int:group_id>/',
+        login_required(ColumnAclList.as_view()),
+        name='qdjango-column-acl-api-filter-by-group'
+    ),
 
     # List field names for a vector layer
-    path('api/column_acl/fields/<int:layer_id>/',
-        login_required(ColumnAclFields.as_view()), name='qdjango-column-acl-api-fields'),
+    path(
+        'api/column_acl/fields/<int:layer_id>/',
+        login_required(ColumnAclFields.as_view()),
+        name='qdjango-column-acl-api-fields'
+    ),
 
     # All Constraint(s)
-    path('api/column_acl/',
-        login_required(ColumnAclList.as_view()), name='qdjango-column-acl-api-list'),
+    path(
+        'api/column_acl/',
+        login_required(ColumnAclList.as_view()),
+        name='qdjango-column-acl-api-list'
+    ),
 
 
     #############################################################
     # GeoConstraints
 
     # Detail of a GeoConstraintRule
-    path('api/georule/detail/<int:pk>/',
-        login_required(GeoConstraintRuleDetail.as_view()), name='geoconstraintrule-api-detail'),
+    path(
+        'api/georule/detail/<int:pk>/',
+        login_required(GeoConstraintRuleDetail.as_view()),
+        name='geoconstraintrule-api-detail'
+    ),
 
     # All ConstraintRule(s) filtered by editing layer id
-    re_path(r'^api/georule/layer/(?P<layer_id>[-_\w\d]+)/$',
-        login_required(GeoConstraintRuleList.as_view()), name='geoconstraintrule-api-filter-by-layer'),
+    re_path(
+        r'^api/georule/layer/(?P<layer_id>[-_\w\d]+)/$',
+        login_required(GeoConstraintRuleList.as_view()),
+        name='geoconstraintrule-api-filter-by-layer'
+    ),
 
     # All ConstraintRule(s) filtered by User pk
-    path('api/georule/user/<int:user_id>/',
-        login_required(GeoConstraintRuleList.as_view()), name='geoconstraintrule-api-filter-by-user'),
+    path(
+        'api/georule/user/<int:user_id>/',
+        login_required(GeoConstraintRuleList.as_view()),
+        name='geoconstraintrule-api-filter-by-user'
+    ),
 
     # All ConstraintRule(s) filtered by Constraint pk
-    path('api/georule/geoconstraint/<int:constraint_id>/',
-        login_required(GeoConstraintRuleList.as_view()), name='geoconstraintrule-api-filter-by-constraint'),
+    path(
+        'api/georule/geoconstraint/<int:constraint_id>/',
+        login_required(GeoConstraintRuleList.as_view()),
+        name='geoconstraintrule-api-filter-by-constraint'
+    ),
 
     # All ConstraintRule(s)
-    path('api/georule/',
-        login_required(GeoConstraintRuleList.as_view()), name='geoconstraintrule-api-list'),
+    path(
+        'api/georule/',
+        login_required(GeoConstraintRuleList.as_view()),
+        name='geoconstraintrule-api-list'
+    ),
 
     # Constraint geometry
-    re_path(r'^api/geoconstraint/geometry/(?P<layer_id>[-_\w\d]+)/$',
-        login_required(GeoConstraintGEOFeatureAPIView.as_view()), name='geoconstraint-api-geometry'),
+    re_path(
+        r'^api/geoconstraint/geometry/(?P<layer_id>[-_\w\d]+)/$',
+        login_required(GeoConstraintGEOFeatureAPIView.as_view()),
+        name='geoconstraint-api-geometry'
+    ),
 
     # Detail of a Constraint
-    path('api/geoconstraint/detail/<int:pk>/',
-        login_required(GeoConstraintDetail.as_view()), name='geoconstraint-api-detail'),
+    path(
+        'api/geoconstraint/detail/<int:pk>/',
+        login_required(GeoConstraintDetail.as_view()),
+        name='geoconstraint-api-detail'
+    ),
 
     # All Constraint(s) filtered by editing layer id
-    re_path(r'^api/geoconstraint/(?P<layer_id>[-_\w\d]+)/$',
-        login_required(GeoConstraintList.as_view()), name='geoconstraint-api-filter-by-layer'),
+    re_path(
+        r'^api/geoconstraint/(?P<layer_id>[-_\w\d]+)/$',
+        login_required(GeoConstraintList.as_view()),
+        name='geoconstraint-api-filter-by-layer'
+    ),
 
     # All Constraint(s)
-    path('api/geoconstraint/',
-        login_required(GeoConstraintList.as_view()), name='geoconstraint-api-list'),
+    path(
+        'api/geoconstraint/',
+        login_required(GeoConstraintList.as_view()),
+        name='geoconstraint-api-list'
+    ),
 
 
     #############################################################
     # OGC (web) services
 
     # All Service(s)
-    path('api/webservice/<int:project_id>/',
-        login_required(QdjangoWebServicesAPIview.as_view()), name='qdjango-webservice-api-list'),
+    path(
+        'api/webservice/<int:project_id>/',
+        login_required(QdjangoWebServicesAPIview.as_view()),
+        name='qdjango-webservice-api-list'
+    ),
 
     #############################################################
     # General API
-    path('api/asgeotiff/<int:project_id>/',
-        QdjangoAsGeoTiffAPIview.as_view(), name='qdjango-asgeotiff-api'),
-    re_path(r'^api/prjtheme/(?P<project_id>\d+)/(?P<theme_name>[-_\w\d\s]+)/$',
-        QdjangoPrjThemeAPIview.as_view(), name='qdjango-prjtheme-api'),
+    path(
+        'api/asgeotiff/<int:project_id>/',
+        QdjangoAsGeoTiffAPIview.as_view(),
+        name='qdjango-asgeotiff-api'
+    ),
+    re_path(
+        r'^api/prjtheme/(?P<project_id>\d+)/(?P<theme_name>[-_\w\d\s]+)/$',
+        QdjangoPrjThemeAPIview.as_view(),
+        name='qdjango-prjtheme-api'
+    ),
 
     # Order
     # ============================================================
-    path('jx/project/<int:project_id>/setorder/', login_required(ProjectSetOrderView.as_view()),
-         name='qdjango-project-set-order'),
+    path(
+        'jx/project/<int:project_id>/setorder/',
+        login_required(ProjectSetOrderView.as_view()),
+        name='qdjango-project-set-order'
+    ),
 
 ]
 
 # Layer style manager
 urlpatterns += [
-    path('api/layerstyles/<int:layer_id>/',
-        login_required(LayerStyleListView.as_view()), name='qdjango-style-list-api'),
-    re_path(r'^api/layerstyles/(?P<layer_id>\d+)/(?P<style_name>[\w\s%-]+)/$',
-        login_required(LayerStyleDetailView.as_view()), name='qdjango-style-detail-api'),
+    path(
+        'api/layerstyles/<int:layer_id>/',
+        login_required(LayerStyleListView.as_view()),
+        name='qdjango-style-list-api'
+    ),
+
+    re_path(
+        r'^api/layerstyles/(?P<layer_id>\d+)/(?P<style_name>[\w\s%-]+)/$',
+        login_required(LayerStyleDetailView.as_view()),
+        name='qdjango-style-detail-api'
+    ),
 ]
 
 # API info
 urlpatterns += [
     # Other vector layers in project get by qdjango layer id
-    re_path(r'^api/info/layer/polygon/(?P<layer_id>[-_\w\d]+)/$',
-        login_required(LayerPolygonView.as_view()), name='qdjango-api-info-layer-polygon'),
+    re_path(
+        r'^api/info/layer/polygon/(?P<layer_id>[-_\w\d]+)/$',
+        login_required(LayerPolygonView.as_view()),
+        name='qdjango-api-info-layer-polygon'
+    ),
+
     # Viewers users can editing on editing layer id
-    re_path(r'^api/info/layer/user/(?P<layer_id>[-_\w\d]+)/$',
-        login_required(LayerUserInfoAPIView.as_view()), name='qdjango-api-info-layer-user'),
+    re_path(
+        r'^api/info/layer/user/(?P<layer_id>[-_\w\d]+)/$',
+        login_required(LayerUserInfoAPIView.as_view()),
+        name='qdjango-api-info-layer-user'
+    ),
+
     # Viewers users groups viewer can editing on editing layer id
-    re_path(r'^api/info/layer/authgroup/(?P<layer_id>[-_\w\d]+)/$',
-        login_required(LayerAuthGroupInfoAPIView.as_view()), name='qdjango-api-info-layer-authgroup'),
+    re_path(
+        r'^api/info/layer/authgroup/(?P<layer_id>[-_\w\d]+)/$',
+        login_required(LayerAuthGroupInfoAPIView.as_view()),
+        name='qdjango-api-info-layer-authgroup'
+    ),
 ]
--- g3w-admin/qdjango/urls.py
+++ g3w-admin/qdjango/urls.py
@@ -6,52 +6,118 @@ from .views import *
 G3W_SITETREE_I18N_ALIAS.append('qdjango')
 
 urlpatterns = [
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/$', login_required(QdjangoProjectListView.as_view()),
-        name='qdjango-project-list'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/project/add/$', login_required(QdjangoProjectCreateView.as_view()),
-        name='qdjango-project-add'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/update/(?P<slug>[-_\w\d]+)/$',
-        login_required(QdjangoProjectUpdateView.as_view()), name='qdjango-project-update'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/delete/(?P<slug>[-_\w\d]+)/$',
-        login_required(QdjangoProjectDeleteView.as_view()), name='qdjango-project-delete'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<slug>[-_\w\d]+)/$',
-        login_required(QdjangoProjectDetailView.as_view()), name='qdjango-project-detail'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/download/(?P<slug>[-_\w\d]+)/$',
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/$',
+        login_required(QdjangoProjectListView.as_view()),
+        name='qdjango-project-list'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/project/add/$',
+        login_required(QdjangoProjectCreateView.as_view()),
+        name='qdjango-project-add'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/update/(?P<slug>[-_\w\d]+)/$',
+        login_required(QdjangoProjectUpdateView.as_view()),
+        name='qdjango-project-update'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/delete/(?P<slug>[-_\w\d]+)/$',
+        login_required(QdjangoProjectDeleteView.as_view()),
+        name='qdjango-project-delete'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<slug>[-_\w\d]+)/$',
+        login_required(QdjangoProjectDetailView.as_view()),
+        name='qdjango-project-detail'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/download/(?P<slug>[-_\w\d]+)/$',
         login_required(QdjangoProjectDownloadView.as_view(model=Project, file_field='qgis_file')),
-        name='qdjango-project-download'),
+        name='qdjango-project-download'
+    ),
 
     # for ajaxFiler
     re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/fast/update/(?P<slug>[-_\w\d]+)/$',
         login_required(QdjangoProjectFastUpdateView.as_view()), name='qdjango-project-fast-update'),
 
+    #############################################################
     # Layers re_paths
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layers/$',
-        login_required(QdjangoLayersListView.as_view()), name='qdjango-project-layers-list'),
-    re_path(r'^jx/(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layers/(?P<layer_id>[0-9]+)/cache/$',
-        login_required(QdjangoLayerCacheView.as_view()), name='qdjango-project-layers-cache'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/$',
-        login_required(QdjangoLayerWidgetsView.as_view()), name='qdjango-project-layer-widgets'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<slug>[-_\w\d]+)/layer/(?P<pk>[0-9]+)/detail/$',
-        login_required(QdjangoLayerDetailView.as_view()), name='qdjango-layer-detail'),
 
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layers/$',
+        login_required(QdjangoLayersListView.as_view()),
+        name='qdjango-project-layers-list'
+    ),
+
+    re_path(
+        r'^jx/(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layers/(?P<layer_id>[0-9]+)/cache/$',
+        login_required(QdjangoLayerCacheView.as_view()),
+        name='qdjango-project-layers-cache'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/$',
+        login_required(QdjangoLayerWidgetsView.as_view()),
+        name='qdjango-project-layer-widgets'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<slug>[-_\w\d]+)/layer/(?P<pk>[0-9]+)/detail/$',
+        login_required(QdjangoLayerDetailView.as_view()),
+        name='qdjango-layer-detail'
+    ),
+
+    #############################################################
     # for data layer by ajax
-    re_path(r'^jx/(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layers/(?P<layer_id>[0-9]+)/data/$',
-        login_required(QdjangoLayerDataView.as_view()), name='qdjango-project-layers-data-editing'),
 
+    re_path(
+        r'^jx/(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layers/(?P<layer_id>[0-9]+)/data/$',
+        login_required(QdjangoLayerDataView.as_view()),
+        name='qdjango-project-layers-data-editing'
+    ),
+
+    #############################################################
     # Widget re_paths
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/add/$',
-        login_required(QdjangoLayerWidgetCreateView.as_view()), name='qdjango-project-layer-widget-add'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/update/(?P<slug>[-_\w\d]+)$',
-        login_required(QdjangoLayerWidgetUpdateView.as_view()), name='qdjango-project-layer-widget-update'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/delete/(?P<slug>[-_\w\d]+)$',
-        login_required(QdjangoLayerWidgetDeleteView.as_view()), name='qdjango-project-layer-widget-delete'),
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/link/(?P<slug>[-_\w\d]+)$',
-        login_required(QdjangoLinkWidget2LayerView.as_view()), name='qdjango-project-layer-widget-link'),
 
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/add/$',
+        login_required(QdjangoLayerWidgetCreateView.as_view()),
+        name='qdjango-project-layer-widget-add'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/update/(?P<slug>[-_\w\d]+)$',
+        login_required(QdjangoLayerWidgetUpdateView.as_view()),
+        name='qdjango-project-layer-widget-update'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/delete/(?P<slug>[-_\w\d]+)$',
+        login_required(QdjangoLayerWidgetDeleteView.as_view()),
+        name='qdjango-project-layer-widget-delete'
+    ),
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/projects/(?P<project_slug>[-_\w\d]+)/layer/(?P<layer_slug>[-_\w\d]+)/widgets/link/(?P<slug>[-_\w\d]+)$',
+        login_required(QdjangoLinkWidget2LayerView.as_view()),
+        name='qdjango-project-layer-widget-link'
+    ),
+
+    #############################################################
     # Filter by user/group re_paths
-    re_path(r'^(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/(?P<layer_id>[0-9]+)/'
+
+    re_path(
+        r'^(?P<group_slug>[-_\w\d]+)/(?P<project_type>[-_\w\d]+)/(?P<project_slug>[-_\w\d]+)/(?P<layer_id>[0-9]+)/'
         r'filterbyuser/$',
-        login_required(FilterByUserLayerView.as_view()), name='fitler-by-user-layer'),
+        login_required(FilterByUserLayerView.as_view()),
+        name='fitler-by-user-layer'
+    ),
 
 ]
 
@@ -62,13 +128,21 @@ try:
     #get qdjango sitetree and add items to core sitetree:
 
     register_dynamic_trees(
-        compose_dynamic_tree('qdjango', target_tree_alias='core', parent_tree_item_alias='project-list',
-                             include_trees=('qdjango',)),
+        compose_dynamic_tree(
+            'qdjango',
+            target_tree_alias='core',
+            parent_tree_item_alias='project-list',
+            include_trees=('qdjango',)
+        ),
         reset_cache=True
     )
     register_dynamic_trees(
-        compose_dynamic_tree('qdjango', target_tree_alias='core_en', parent_tree_item_alias='project-list',
-                             include_trees=('qdjango_en',)),
+        compose_dynamic_tree(
+            'qdjango',
+            target_tree_alias='core_en',
+            parent_tree_item_alias='project-list',
+            include_trees=('qdjango_en',)
+        ),
         reset_cache=True
     )
 
