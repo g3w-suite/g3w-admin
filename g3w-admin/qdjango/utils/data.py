6755bf3283ecc1dc416dbb64bf20334e5e32f3b3
wlorenzetti <lorenzett@gis3w.it>
2022-12-14 08:39:26 +0100

    Rollback of fix of issue #426 from dev branch.

--- g3w-admin/qdjango/utils/data.py
+++ g3w-admin/qdjango/utils/data.py
@@ -1368,8 +1368,8 @@ class QgisProject(XmlData):
                              'download_csv',
                              'download_gpkg',
                              'external',
-                             'not_show_attributes_table'):
-                            print(embedded['id'], p, getattr(existing_layer, p))
+                             'not_show_attributes_table',
+                             'has_column_acl'):
                             setattr(embedded_layer, p, getattr(existing_layer, p))
 
                     except Layer.DoesNotExist:
