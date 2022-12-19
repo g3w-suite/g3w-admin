22c2b99b65c8668d4b0c48d6490fb718c4094093
wlorenzetti <lorenzett@gis3w.it>
2022-12-19 10:36:22 +0100

    Get max bounds of crs.

--- g3w-admin/core/api/serializers.py
+++ g3w-admin/core/api/serializers.py
@@ -10,8 +10,9 @@ from rest_framework.fields import empty
 from core.models import Group, BaseLayer
 from core.signals import initconfig_plugin_start
 from core.mixins.api.serializers import G3WRequestSerializer
+from core.utils.geo import get_crs_bbox
 
-from qgis.core import QgsCoordinateReferenceSystem
+from qgis.core import QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsCoordinateTransformContext
 from copy import copy
 
 
@@ -106,7 +107,8 @@ class GroupSerializer(G3WRequestSerializer, serializers.ModelSerializer):
             'epsg': crs.postgisSrid(),
             'proj4': proj4,
             'geographic': crs.isGeographic(),
-            'axisinverted': crs.hasAxisInverted()
+            'axisinverted': crs.hasAxisInverted(),
+            'extent': get_crs_bbox(crs)
         }
 
         # map controls
