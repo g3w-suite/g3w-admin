783be865ddd93a76967e0f2186af29c96b80ca79
wlorenzetti <lorenzett@gis3w.it>
2023-01-31 17:16:59 +0100

    Porting PR #447.

--- g3w-admin/qdjango/utils/data.py
+++ g3w-admin/qdjango/utils/data.py
@@ -775,6 +775,20 @@ class QgisProjectLayer(XmlData):
             #         'duration_field': tp.durationField()
             #     }
 
+        # Get step and set-unit from ProjectTimeSettings property
+        # QGIS desktop save last temporal settings
+        if toret:
+            ts = self.qgisProject.qgs_project.timeSettings()
+            toret.update({
+                'step': ts.timeStep()
+            })
+
+            if toret['mode'] != 'FeatureDateTimeInstantFromField':
+                toret.update({
+                    'units': QgsUnitTypes.encodeUnit(ts.timeStepUnit())
+                })
+
+
         return json.dumps(toret)
 
     def clean(self):
