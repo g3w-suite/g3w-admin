e310b91ebd138bc4da1ecec0b2b58c12ab28d042
wlorenzetti <lorenzett@gis3w.it>
2023-03-07 08:00:05 +0100

    Fix count total group by user for dashboard

--- g3w-admin/core/views.py
+++ g3w-admin/core/views.py
@@ -63,9 +63,10 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         # add number groups
-        qs = get_objects_for_user(self.request.user, 'core.view_group', Group) | \
-             get_objects_for_user(get_anonymous_user(), 'core.view_group', Group)
-        qs = qs.order_by('order')
+        qs = get_objects_for_user(self.request.user, 'core.view_group', Group)
+             #| get_objects_for_user(get_anonymous_user(), 'core.view_group', Group)
+
+        qs = qs.filter(is_active=1).order_by('order')
         context['n_groups'] = len(qs)
         context['widgets'] = []
 
