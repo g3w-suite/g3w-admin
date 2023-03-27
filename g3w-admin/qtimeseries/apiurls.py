26921741934aeb2c2b78ee217942c75f5fd96db4
Raruto <Raruto@users.noreply.github.com>
2023-03-27 14:15:32 +0200

    remove `/api/` prefix (ref: `apiurls.py`)
    it is a convention used in various modules (ie. to have `<app_name>/api/` prefix), but it is not strictly mandatory

--- g3w-admin/qtimeseries/apiurls.py
+++ g3w-admin/qtimeseries/apiurls.py
@@ -1,7 +1,7 @@
 """
 Add your API routes here.
 """
-# API ROOT: /qtimeseries/api/
+# API ROOT: /qtimeseries/
 
 __author__    = 'lorenzetti@gis3w.it'
 __copyright__ = 'Copyright 2015 - 2023, Gis3w'
