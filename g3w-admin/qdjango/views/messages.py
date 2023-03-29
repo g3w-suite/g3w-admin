c13abd17e8f8814c3ee7db6a2eaab0015fdb7494
wlorenzetti <lorenzett@gis3w.it>
2023-03-29 15:53:09 +0200

    Refactoring: organize qdjango views.

---
+++ g3w-admin/qdjango/views/messages.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+"""" Messagin system views
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2023-03-29'
+__copyright__ = 'Copyright 2015 - 2023, Gis3w'
+__license__ = 'MPL 2.0'
