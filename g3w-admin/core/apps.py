2ca1f489e6507b29dde418ea15c968f37baa0f7b
wlorenzetti <lorenzett@gis3w.it>
2023-03-29 10:53:05 +0200

    Remove `required` for model field `Group.header_logo_img` create basic workflow for header_logo_img.

--- g3w-admin/core/apps.py
+++ g3w-admin/core/apps.py
@@ -1,7 +1,16 @@
-
-
 from django.apps import AppConfig
+from django.conf import settings
+import os
+import shutil
 
 class CoreConfig(AppConfig):
     name = 'core'
     verbose_name = 'G3W-Admin main app'
+
+    # For default Group logo image
+    # -----------------------------------------------------------------
+    f = settings.CLIENT_G3WSUITE_LOGO
+    frm = f"{os.path.dirname(__file__)}/static/img/{f}"
+    dst = f"{settings.MEDIA_ROOT}logo_img/"
+    if not os.path.exists(f"{dst}{f}"):
+        shutil.copy(frm, dst)
