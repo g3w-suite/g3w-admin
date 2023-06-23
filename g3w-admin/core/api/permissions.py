5a7673bf4fd0cdde41cfb1b558aa1a5c0e2c5951
wlorenzetti <lorenzett@gis3w.it>
2023-06-23 10:27:17 +0200

    Refactoring.

--- g3w-admin/core/api/permissions.py
+++ g3w-admin/core/api/permissions.py
@@ -2,7 +2,6 @@ from django.apps import apps
 from rest_framework.permissions import BasePermission
 from guardian.utils import get_anonymous_user
 from core.models import Group
-from qdjango.models import Project
 
 
 class ProjectPermission(BasePermission):
@@ -15,11 +14,11 @@ class ProjectPermission(BasePermission):
         # get model by type
         func, args, kwargs = request.resolver_match
 
-        if 'project_type' in kwargs:
-            Model = apps.get_app_config(kwargs['project_type']).get_model('project')
-            project = Model.objects.get(pk=kwargs['project_id'])
-        else:
-            project = Project.objects.get(pk=kwargs['project_id'])
+        if 'project_type' not in kwargs:
+            kwargs['project_type'] = 'qdjango'
+
+        Project = apps.get_app_config(kwargs['project_type']).get_model('project')
+        project = Project.objects.get(pk=kwargs['project_id'])
         return request.user.has_perm('{}.view_project'.format(kwargs['project_type']), project) or \
             get_anonymous_user().has_perm('{}.view_project'.format(kwargs['project_type']), project)
 
