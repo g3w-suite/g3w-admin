d8d6e99439bf64f65a83377e480c8509f213aa9c
wlorenzetti <lorenzett@gis3w.it>
2023-06-23 10:14:56 +0200

    API ProjectPermission for url with only `project_id` parameter.

--- g3w-admin/core/api/permissions.py
+++ g3w-admin/core/api/permissions.py
@@ -2,6 +2,7 @@ from django.apps import apps
 from rest_framework.permissions import BasePermission
 from guardian.utils import get_anonymous_user
 from core.models import Group
+from qdjango.models import Project
 
 
 class ProjectPermission(BasePermission):
@@ -14,8 +15,11 @@ class ProjectPermission(BasePermission):
         # get model by type
         func, args, kwargs = request.resolver_match
 
-        Project = apps.get_app_config(kwargs['project_type']).get_model('project')
-        project = Project.objects.get(pk=kwargs['project_id'])
+        if 'project_type' in kwargs:
+            Model = apps.get_app_config(kwargs['project_type']).get_model('project')
+            project = Model.objects.get(pk=kwargs['project_id'])
+        else:
+            project = Project.objects.get(pk=kwargs['project_id'])
         return request.user.has_perm('{}.view_project'.format(kwargs['project_type']), project) or \
             get_anonymous_user().has_perm('{}.view_project'.format(kwargs['project_type']), project)
 
