6c6a02fda1acda926815561f2bf8c8f70ab3d421
wlorenzetti <lorenzett@gis3w.it>
2023-09-14 12:05:10 +0200

    Base registration two-step workflow.

---
+++ g3w-admin/usersmanage/receivers.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+"""" Usermanage receivers module
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = "lorenzetti@gis3w.it"
+__date__ = "2023-09-14"
+__copyright__ = "Copyright 2015 - 2023, Gis3w"
+__license__ = "MPL 2.0"
+
+from django.conf import settings
+from django.dispatch import receiver
+from django_registration.signals import user_registered
+from usersmanage.models import Userbackend, USER_BACKEND_DEFAULT, Group as AuthGroup
+from usersmanage.configs import G3W_VIEWER1
+import logging
+
+logger = logging.getLogger('django.request')
+
+@receiver(user_registered)
+def set_user_backend(sender, **kwargs):
+    """
+    Set User-backend on registration
+    """
+
+    # Set default g3w-suite backend
+    Userbackend(user=kwargs['user'], backend=USER_BACKEND_DEFAULT).save()
+    logger.info(f"Assigned backend {USER_BACKEND_DEFAULT} to registered user {kwargs['user']}")
+
+    # Set default registration role
+    if hasattr(settings, 'REGISTRATION_MAIN_ROLES'):
+        for role in settings.REGISTRATION_MAIN_ROLES:
+            AuthGroup.objects.get(name=role).user_set.add(kwargs["user"])
+    else:
+
+        # By default, add user to Viewer Level 1 group
+        AuthGroup.objects.get(name=G3W_VIEWER1).user_set.add(kwargs['user'])
+
