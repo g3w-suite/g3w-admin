01c3d9bfaa1d3d22ac58c486fc36bb95c32eb277
wlorenzetti <lorenzett@gis3w.it>
2023-11-27 09:36:55 +0100

    Typo

--- g3w-admin/qdjango/migrations/0109_project_wms_getmap_format.py
+++ g3w-admin/qdjango/migrations/0109_project_wms_getmap_format.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('qdjango', '0109_filterlayersaved.py'),
+        ('qdjango', '0109_filterlayersaved'),
     ]
 
     operations = [
