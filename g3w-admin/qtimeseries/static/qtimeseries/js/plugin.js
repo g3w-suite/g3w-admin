adc35b3816bf3b3f2a3e55d5bb1dcc52e7666d1b
Raruto <Raruto@users.noreply.github.com>
2024-02-02 15:16:38 +0100

    revert 4a20d4e2af81c1e0fa3b4c8daa0ec702354f74e7

--- g3w-admin/qtimeseries/static/qtimeseries/js/plugin.js
+++ g3w-admin/qtimeseries/static/qtimeseries/js/plugin.js
@@ -18,96 +18,93 @@
       const VM = new Vue();
       const i18n = async lang => this.setLocale({ [lang]: (await import(BASE_URL + '/i18n/' + lang + '.js')).default });
       VM.$watch(() => ApplicationState.language, i18n);
-      //need to wait promise resolve from
-      i18n(ApplicationState.language)
-        .then(()=> {
-          const enabled = this.registerPlugin(this.config.gid);
+      i18n(ApplicationState.language);
 
-          this.setHookLoading({ loading: true });
+      const enabled = this.registerPlugin(this.config.gid);
 
-          const project = ApplicationService.getCurrentProject();
+      this.setHookLoading({ loading: true });
 
-          // add project layers from config
-          project
-            .getConfigLayers()
-            .filter(l => 'Object' === toRawType(l.qtimeseries))
-            .forEach(l => {
-              let {
-                units      = 'd',
-                start_date = null,
-                end_date   = null,
-              } = l.qtimeseries;
+      const project = ApplicationService.getCurrentProject();
 
-              start_date = moment(start_date).add(new Date(start_date).getTimezoneOffset(), 'minutes');
-              end_date   = moment(end_date).add(new Date(end_date).getTimezoneOffset(), 'minutes');
+      // add project layers from config
+      project
+        .getConfigLayers()
+        .filter(l => 'Object' === toRawType(l.qtimeseries))
+        .forEach(l => {
+          let {
+            units      = 'd',
+            start_date = null,
+            end_date   = null,
+          } = l.qtimeseries;
 
-              const u          = this.config.steps.find(step_unit => step_unit.qgis === units).moment.split(':');
-              const multiplier = u.length > 1 ? 1 * u[0] : 1;
-              const step_unit  = u.length > 1 ? u[1] : u[0];
-              const layer      = project.getLayerById(l.id);
+          start_date = moment(start_date).add(new Date(start_date).getTimezoneOffset(), 'minutes');
+          end_date   = moment(end_date).add(new Date(end_date).getTimezoneOffset(), 'minutes');
 
-              this.config.layers.push({
-                id:                   l.id,
-                name:                 layer.getName(),
-                wmsname:              layer.getWMSLayerName(),
-                start_date,
-                end_date,
-                options: {
-                  range_max:          moment(end_date).diff(moment(start_date), step_unit) - 1,
-                  step:               l.qtimeseries.step ?? 1,
-                  stepunit:           step_unit,
-                  stepunitmultiplier: multiplier,
-                  field:              l.qtimeseries.field,
-                }
-              });
-            });
+          const u          = this.config.steps.find(step_unit => step_unit.qgis === units).moment.split(':');
+          const multiplier = u.length > 1 ? 1 * u[0] : 1;
+          const step_unit  = u.length > 1 ? u[1] : u[0];
+          const layer      = project.getLayerById(l.id);
 
-          // inizialize service
-          service.config = this.config;
-          service.toggle = (state) => { service.open = state ?? !service.open; }
-          service.close  = () => { service.open = false; }
-
-          this.on('unload', () => service.close())
-
-          const show = this.config.layers.length > 0;
-
-          service.emit('ready', show);
-
-          if (show) {
-            service.open = false;
-          }
-
-          // setup plugin interface
-          GUI.isReady().then(() => {
-            // skip when ...
-            if(!enabled || !show) {
-              return;
+          this.config.layers.push({
+            id:                   l.id,
+            name:                 layer.getName(),
+            wmsname:              layer.getWMSLayerName(),
+            start_date,
+            end_date,
+            options: {
+              range_max:          moment(end_date).diff(moment(start_date), step_unit) - 1,
+              step:               l.qtimeseries.step ?? 1,
+              stepunit:           step_unit,
+              stepunitmultiplier: multiplier,
+              field:              l.qtimeseries.field,
             }
+          });
+        });
 
-            this.createSideBarComponent({},
-              {
-                ...this.config.sidebar,
-                id: this.name,
-                events: {
-                  open: {
-                    when: 'before',
-                    cb: bool => {
-                      this._panel = this._panel || new (Vue.extend({
-                        functional: true,
-                        components: { 'qts': httpVueLoader(BASE_URL + '/plugin.vue')},
-                        render: h => h('qts', { props: { service } }),
-                      }))().$mount(ApplicationService.getService('sidebar').getComponent('qtimeseries').getInternalComponent().$el);
-                      service.toggle(bool);
-                    }
-                  }
+      // inizialize service
+      service.config = this.config;
+      service.toggle = (state) => { service.open = state ?? !service.open; }
+      service.close  = () => { service.open = false; }
+
+      this.on('unload', () => service.close())
+
+      const show = this.config.layers.length > 0;
+
+      service.emit('ready', show);
+
+      if (show) {
+        service.open = false;
+      }
+
+      // setup plugin interface
+      GUI.isReady().then(() => {
+        // skip when ...
+        if(!enabled || !show) {
+          return;
+        }
+
+        this.createSideBarComponent({},
+          {
+            ...this.config.sidebar,
+            id: this.name,
+            events: {
+              open: {
+                when: 'before',
+                cb: bool => {
+                  this._panel = this._panel || new (Vue.extend({
+                    functional: true,
+                    components: { 'qts': httpVueLoader(BASE_URL + '/plugin.vue')},
+                    render: h => h('qts', { props: { service } }),
+                  }))().$mount(ApplicationService.getService('sidebar').getComponent('qtimeseries').getInternalComponent().$el);
+                  service.toggle(bool);
                 }
               }
-            );
+            }
+          }
+        );
 
-            this.setReady(true);
-          });
-
-        })
+        this.setReady(true);
+      });
 
       this.setHookLoading({ loading: false });
     }
