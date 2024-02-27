2f491be671c5eeb3b103be03e72df20479dabd8b
wlorenzetti <lorenzett@gis3w.it>
2024-02-27 18:00:03 +0100

    Typo: fix 'nomacrogroup' filter.

--- g3w-admin/about/api/filters.py
+++ g3w-admin/about/api/filters.py
@@ -53,8 +53,12 @@ class GroupProjectFilter(BaseFilterBackend):
         """
         Return a filtered queryset by group_id
         """
+
         if 'group_id' in view.kwargs:
             queryset = queryset.filter(group_id=view.kwargs['group_id']).order_by('order')
+
+        if url_name == 'about-group-without-macrogroup-api-list':
+            queryset = queryset.filter(macrogroups__pk=None)
 
         return queryset
 
@@ -70,7 +74,7 @@ class MacroGroupGroupFilter(BaseFilterBackend):
             queryset = queryset.filter(macrogroups__pk=view.kwargs['macrogroup_id'])
 
         # check for group without macrogroup
-        if resolve(request.path_info).url_name == 'portal-group-without-macrogroup-api-list':
+        if resolve(request.path_info).url_name == 'about-group-without-macrogroup-api-list':
             queryset = queryset.filter(macrogroups__pk=None)
 
         return queryset
