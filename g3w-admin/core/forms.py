05f81932b239833c9cfaef35e763a4da052f206d
Raruto <Raruto@users.noreply.github.com>
2025-03-19 11:02:01 +0100

    skip-icheck

--- g3w-admin/core/forms.py
+++ g3w-admin/core/forms.py
@@ -88,6 +88,7 @@ class GroupForm(TranslationModelForm, FileFormMixin, G3WFormMixin, G3WRequestFor
                 'upload_url',
                 #AppendedText('header_logo_height','px'),
                 'header_logo_link',
+                css_class='skip-icheck'
             ),
 
             Fieldset(
