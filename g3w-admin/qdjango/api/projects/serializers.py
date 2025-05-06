a80f6052937d03cd8457bb72cd766e1984e70fe1
Raruto <Raruto@users.noreply.github.com>
2025-05-06 09:56:40 +0200

    `CLIENT_CUSTOM_CSS` instead of `/api/config`

--- g3w-admin/qdjango/api/projects/serializers.py
+++ g3w-admin/qdjango/api/projects/serializers.py
@@ -569,8 +569,6 @@ class ProjectSerializer(G3WRequestSerializer, serializers.ModelSerializer):
             ret['relations'] += self.get_map_layers_relations(instance)
 
         # Add project metadata
-        # Also if show or not to show metadata
-        ret['show_metadata_section'] = instance.show_metadata_section
         ret['metadata'] = self.get_metadata(instance, qgs_project)
 
         # set client options/actions
