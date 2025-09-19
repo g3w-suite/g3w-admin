eeccc90ea6517e197f380546b3124e37a557c539
Raruto <Raruto@users.noreply.github.com>
2025-09-19 10:22:27 +0200

    `ol.color.asString` → `rgba()`

--- g3w-admin/editing/static/editing/js/utils/setFeaturesSelectedStyle.js
+++ g3w-admin/editing/static/editing/js/utils/setFeaturesSelectedStyle.js
@@ -4,7 +4,6 @@
  * Set selected style to feature and return original feature style
  * 
  * @param { Array } features
- * @param { ol.style.Style } style  @since 3.8.0 custom select style
  * 
  * @returns { ol.style.Style }
  */
@@ -12,19 +11,18 @@ export function setFeaturesSelectedStyle(features = []) {
   if (features.length > 0) {
     const feats = features.flat();     // flat nested features
     let style   = feats[0].getStyle(); // selected style
-    const color = 'rgb(255,255,0)';
 
     if (['LineString', 'MultiLineString'].includes(feats[0].getGeometry())) {
-      style = new ol.style.Style({ stroke: new ol.style.Stroke({ color, width: 4 }) });
+      style = new ol.style.Style({ stroke: new ol.style.Stroke({ color: 'rgb(255,255,0)', width: 4 }) });
     }
     if (['Point', 'MultiPoint'].includes(feats[0].getGeometry())) {
-      style = new ol.style.Style({ image: new ol.style.Circle({ radius: 6, fill: new ol.style.Fill({ color }) }), zIndex: Infinity });
+      style = new ol.style.Style({ image: new ol.style.Circle({ radius: 6, fill: new ol.style.Fill({ color: 'rgb(255,255,0)' }) }), zIndex: Infinity });
     }
     if (['Polygon', 'MultiPolygon'].includes(feats[0].getGeometry())) {
-      style = new ol.style.Style({ stroke: new ol.style.Stroke({ color, width: 4 }), fill: new ol.style.Fill({ color: ol.color.asString([...ol.color.asArray(color)].splice(0, 3).concat(.25)) }) /* force rgba color transparency (alpha = .25) */ });
+      style = new ol.style.Style({ stroke: new ol.style.Stroke({ color: 'rgb(255,255,0)', width: 4 }), fill: new ol.style.Fill({ color: 'rgba(255,255,0,0.25)' }) });
     }
 
-    feats.forEach(f => f.setStyle(style || style));
+    feats.forEach(f => f.setStyle(style));
 
     return feats[0].getStyle();
   }
