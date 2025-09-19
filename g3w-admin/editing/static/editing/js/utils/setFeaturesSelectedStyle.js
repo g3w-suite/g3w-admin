0f8a5bbee39f4498db777bc62aac41682326461a
Raruto <Raruto@users.noreply.github.com>
2025-09-19 10:17:01 +0200

    remove from core: `g3wsdk.core.geoutils.createSelectedStyle`

--- g3w-admin/editing/static/editing/js/utils/setFeaturesSelectedStyle.js
+++ g3w-admin/editing/static/editing/js/utils/setFeaturesSelectedStyle.js
@@ -1,24 +1,6 @@
 /**
  * ORIGINAL SOURCE: g3w-client-plugin-editing/workflows/tasks/editingtask.js@v3.7.1
  * 
- * Get selected style from "extracted" original feature style  
- * 
- * @param feature
- * 
- * @returns {{ originalStyle: *, selectedStyle: * }} selected style based on a geometry type
- */
-function getSelectedStyle(feature) {
-  return {
-    originalStyle: feature.getStyle(),
-    selectedStyle: feature.getGeometry()
-    ? g3wsdk.core.geoutils.createSelectedStyle({ geometryType: feature.getGeometry().getType() })
-    : feature.getStyle()
-  }
-}
-
-/**
- * ORIGINAL SOURCE: g3w-client-plugin-editing/workflows/tasks/editingtask.js@v3.7.1
- * 
  * Set selected style to feature and return original feature style
  * 
  * @param { Array } features
@@ -26,14 +8,24 @@ function getSelectedStyle(feature) {
  * 
  * @returns { ol.style.Style }
  */
-export function setFeaturesSelectedStyle(features = [], style) {
+export function setFeaturesSelectedStyle(features = []) {
   if (features.length > 0) {
-    // copy feature from other layers when selecting multiple features
-    const arr                              = features.flat(); // flat nested features
-    const { originalStyle, selectedStyle } = getSelectedStyle(arr[0]);
+    const feats = features.flat();     // flat nested features
+    let style   = feats[0].getStyle(); // selected style
+    const color = 'rgb(255,255,0)';
 
-    arr.forEach(f => f.setStyle(style || selectedStyle));
+    if (['LineString', 'MultiLineString'].includes(feats[0].getGeometry())) {
+      style = new ol.style.Style({ stroke: new ol.style.Stroke({ color, width: 4 }) });
+    }
+    if (['Point', 'MultiPoint'].includes(feats[0].getGeometry())) {
+      style = new ol.style.Style({ image: new ol.style.Circle({ radius: 6, fill: new ol.style.Fill({ color }) }), zIndex: Infinity });
+    }
+    if (['Polygon', 'MultiPolygon'].includes(feats[0].getGeometry())) {
+      style = new ol.style.Style({ stroke: new ol.style.Stroke({ color, width: 4 }), fill: new ol.style.Fill({ color: ol.color.asString([...ol.color.asArray(color)].splice(0, 3).concat(.25)) }) /* force rgba color transparency (alpha = .25) */ });
+    }
 
-    return originalStyle;
+    feats.forEach(f => f.setStyle(style || style));
+
+    return feats[0].getStyle();
   }
 }
