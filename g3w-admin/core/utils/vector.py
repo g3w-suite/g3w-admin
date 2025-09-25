231aa4c4c9bd94f5e48ea953d6f73d93bc644371
wlorenzetti <lorenzetti@gis3w.it>
2025-09-25 17:49:07 +0200

    Not override user media on editing

--- g3w-admin/core/utils/vector.py
+++ g3w-admin/core/utils/vector.py
@@ -115,6 +115,19 @@ class BaseUserMediaHandler(object):
 
                 path_to_save = self.get_path_to_save()
                 path_file_to_save = '{}/{}'.format(path_to_save, file_name)
+                
+                # Check if file already exists, add suffix if needed
+                if os.path.exists(path_file_to_save):
+                    base, ext = os.path.splitext(file_name)
+                    count = 1
+                    while True:
+                        new_file_name = f"{base}_{count}{ext}"
+                        new_path_file_to_save = '{}/{}'.format(path_to_save, new_file_name)
+                        if not os.path.exists(new_path_file_to_save):
+                            file_name = new_file_name
+                            path_file_to_save = new_path_file_to_save
+                            break
+                        count += 1
 
                 # Check if it is a user-media view
                 url_path = urllib.parse.urlparse(self.feature_properties[field]).path
@@ -161,8 +174,8 @@ class BaseUserMediaHandler(object):
                         # path to save media file
                         path_to_file_tmp = BaseUserMediaHandler.build_fs_path(self.feature_properties[field])
 
-                        if not os.path.isdir(path_to_save):
-                            os.makedirs(path_to_save)
+                        # if not os.path.isdir(path_to_save):
+                        #     os.makedirs(path_to_save)
 
                         # for upload of the same image or doc during parallel attribute editing
                         # move delete fo temp uploaded files at the end of commit workflow.
