76c3bba553a2e9bb6430727973d5bbe6bdb710d1
Walter Lorenzetti <lorenzetti@gis3w.it>
2025-09-30 15:55:37 +0200

    Fix features download for layer relation for CSV format (#1217)
    * Typo
    
    * Fix not shortName property
    
    (cherry picked from commit 0212932ab0b4249e2206a3b2c25d833f3ad360e0)

--- g3w-admin/qdjango/utils/data.py
+++ g3w-admin/qdjango/utils/data.py
@@ -219,8 +219,11 @@ class QgisProjectLayer(XmlData):
         :rtype: str
         """
 
-        name = self.qgs_layer.shortName()
-        if not name:
+        try:
+            name = self.qgs_layer.serverProperties().shortName()
+            if not name:
+                name = self.qgs_layer.name()
+        except:
             name = self.qgs_layer.name()
         return name
 
