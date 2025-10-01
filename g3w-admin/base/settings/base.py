718035e4a49f54721c161d4db77a082ae15d34bf
wlorenzetti <lorenzetti@gis3w.it>
2025-10-01 15:34:34 +0200

    Add CSP and CORS middleware for iframe support; update requirements

--- g3w-admin/base/settings/base.py
+++ g3w-admin/base/settings/base.py
@@ -72,6 +72,8 @@ THIRD_PARTY_APPS = [
     "allauth.socialaccount.providers.microsoft",
     "allauth.socialaccount.providers.google",
     "allauth.usersessions",
+    "csp", # Used for Content Security Policy for G3W-SUITE inside iframe
+    "corsheaders" # Used for G3W-SUITE inside iframe
 ]
 
 G3WADMIN_APPS = [
@@ -93,6 +95,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+
+    # CSP and CORS
+    "csp.middleware.CSPMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
