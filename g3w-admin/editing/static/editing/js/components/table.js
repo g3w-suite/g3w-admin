ee1e44e603231393dc6a19bc6cde27b28a22dab0
Raruto <Raruto@users.noreply.github.com>
2025-11-05 14:57:29 +0100

    values per page

--- g3w-admin/editing/static/editing/js/components/table.js
+++ g3w-admin/editing/static/editing/js/components/table.js
@@ -29,7 +29,7 @@ export default ({
 
   <div style="display: flex;">
     <!-- PAGE SIZE -->
-    <label style="margin-top: 5px;">{{ $t('show') }} <select style = "border: 1px solid #aaa;" v-model = "search.page_size">
+    <label style="margin-top: 5px;"><select style = "border: 1px solid #aaa;" v-model = "search.page_size">
       <option v-for = "l in PAGELENGTHS" :value = "l">{{ l }}</option>
     </select> {{ $t('values per page') }}</label>
 
