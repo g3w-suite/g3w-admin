e9a00a38ca2dae0e079023edf9a2a0dff2c74880
Raruto <Raruto@users.noreply.github.com>
2025-11-05 14:37:25 +0100

    simplify pagination

--- g3w-admin/editing/static/editing/js/components/relation.js
+++ g3w-admin/editing/static/editing/js/components/relation.js
@@ -291,7 +291,6 @@ export default ({
 
         <!-- PAGINATION BUTTONS -->
         <div style = "margin-left: auto;" >
-          <button @click.stop = "search.page = Number(search.page) - 1" class="btn" v-disabled = "1 == search.page">«</button>
           <select
             v-model         = "search.page"
             style           = "padding: 5px 12px; appearance: none; border: 0; text-align: center; border-radius: 3px; cursor: pointer;"
@@ -300,7 +299,9 @@ export default ({
           >
             <option v-for = "p in pages" :selected = "p == search.page">{{ p }}</option>
           </select>
-          <button @click.stop = "search.page = Number(search.page) + 1" class="btn" v-disabled = "pages == search.page">»</button>
+          {{ $t(' of ') + pages }}
+          <button title="Backward" data-placement="top" @click.stop = "search.page = Number(search.page) - 1" class="btn" v-disabled = "1 == search.page">🞀</button>
+          <button title="Forward"  data-placement="top" @click.stop = "search.page = Number(search.page) + 1" class="btn" v-disabled = "pages == search.page">🞂</button>
         </div>
       </div>
     </div>
--- g3w-admin/editing/static/editing/js/components/table.js
+++ g3w-admin/editing/static/editing/js/components/table.js
@@ -146,7 +146,6 @@ export default ({
 
     <!-- PAGINATION BUTTONS -->
     <div style = "margin-left: auto;" >
-      <button @click.stop = "search.page = Number(search.page) - 1" class="btn" v-disabled = "1 == search.page">«</button>
       <select
         v-model         = "search.page"
         style           = "padding: 5px 12px; appearance: none; border: 0; text-align: center; border-radius: 3px; cursor: pointer;"
@@ -155,7 +154,9 @@ export default ({
       >
         <option v-for = "p in pages" :selected = "p == search.page">{{ p }}</option>
       </select>
-      <button @click.stop = "search.page = Number(search.page) + 1" class="btn" v-disabled = "pages == search.page">»</button>
+      {{ $t(' of ') + pages }}
+      <button title="Backward" data-placement="top" @click.stop = "search.page = Number(search.page) - 1" class="btn" v-disabled = "1 == search.page">🞀</button>
+      <button title="Forward"  data-placement="top" @click.stop = "search.page = Number(search.page) + 1" class="btn" v-disabled = "pages == search.page">🞂</button>
     </div>
   </div>
 
