45d5ba74d95ed3c4cfc641251a4e863143c38906
wlorenzetti <lorenzetti@gis3w.it>
2026-03-10 17:00:56 +0100

    Update project model fields: set 'autozoom_query' default to True and 'show_metadata_section' default to False

--- g3w-admin/qdjango/models/projects.py
+++ g3w-admin/qdjango/models/projects.py
@@ -364,13 +364,13 @@ class Project(G3WProjectMixins, G3WACLModelMixins, TimeStampedModel):
 
     autozoom_query = models.BooleanField(
         _('Automatic zoom to query result features'),
-        default=False,
+        default=True,
         help_text=_('Automatic zoom on query result features for only one layer'),
     )
 
     show_metadata_section = models.BooleanField(
         _("Show the 'Metadata' section on left bar"),
-        default=True,
+        default=False,
         help_text=_("It is possible choose if show or hide the 'Metadata' section on left bar"),
     )
 
