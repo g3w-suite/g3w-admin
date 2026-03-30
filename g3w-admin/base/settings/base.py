0ef075c3eb0328335e4261e49e60c2b0b2713568
wlorenzetti <lorenzetti@gis3w.it>
2026-03-30 11:43:10 +0200

    Add reserved print layout labels for user's first and last names

--- g3w-admin/base/settings/base.py
+++ g3w-admin/base/settings/base.py
@@ -497,7 +497,9 @@ QES_INDEXING_FIELDS = {}
 # Reserved QGIS print layout labels
 RESERVED_PRINT_LAYOUT_LABELS = [
     'g3w_username', # Reserved for the username of the logged in user
-    'g3w_user_email' # Reserved for the email of the logged in user
+    'g3w_user_email', # Reserved for the email of the logged in user
+    'g3w_user_first_name', # Reserved for the first name of the logged in user
+    'g3w_user_last_name' # Reserved for the last name of the logged in user
 ]
 
 
