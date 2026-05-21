367b45a5877cd26c4c75fd7a5dd596cda0551d58
Raruto <Raruto@users.noreply.github.com>
2026-05-21 14:48:04 +0200

    refactor: remove outer promise and `handlerAfterSessionGetFeatures` function

--- g3w-admin/editing/static/editing/js/g3w-toolbox.js
+++ g3w-admin/editing/static/editing/js/g3w-toolbox.js
@@ -1820,163 +1820,160 @@ export class ToolBox extends Emitter {
    * 
    * @returns { Promise<unknown> } info about start editing has features loaded
    */
-  start(options = {}) {
-    return new Promise(async (resolve, reject) => {
+  async start(options = {}) {
 
-      try {
-        // get current style of layer
-        this.#current_style = this.state.layer.getCurrentStyle().name;
+    try {
+      // get current style of layer
+      this.#current_style = this.state.layer.getCurrentStyle().name;
 
-        const plugin = GUI.getPlugin('editing');
-        const id     = this.getId();
+      const plugin = GUI.getPlugin('editing');
+      const id     = this.getId();
 
-        plugin.state.showselectlayers = options.showselectlayers ?? true;
-        plugin.state.toolboxselected  = (options.selected ?? true) ? this : plugin.state.toolboxselected;
+      plugin.state.showselectlayers = options.showselectlayers ?? true;
+      plugin.state.toolboxselected  = (options.selected ?? true) ? this : plugin.state.toolboxselected;
 
-        const constraints = plugin.state.constraints.toolboxes[id];
+      const constraints = plugin.state.constraints.toolboxes[id];
 
-        // set title
-        if (undefined !== options.title) {
-          this.setTitle(options.title);
-        }
-
-        this.state.changingtools = options.changingtools ?? false;
-
-        if (options.tools) {
-          this.setEnablesDisablesTools(options.tools);
-        }
-
-        this.state.toolboxheader    = options.toolboxheader ?? true;
-        this.state.startstopediting = options.startstopediting ?? true;
-    
-        options.filter = constraints?.filter || this.constraints.filter || options.filter;
-
-        // register lock features to show a message
-        const unKeyLock = this._editor.onceafter('featuresLockedByOtherUser', () => {
-          GUI.showUserMessage({
-            type:     'warning',
-            subtitle: this.state.layer.getName().toUpperCase(),
-            message:  'plugins.editing.messages.featureslockbyotheruser',
-          })
-        });
-    
-        // add featuresLockedByOtherUser setter
-        this.state._unsetters.push(() => this._editor.un('featuresLockedByOtherUser', unKeyLock));
-
-        // check if can we edit based on scale contraint (vector layer)
-        if (this.state._constraints.scale) {
-          const { promise, resolve: res, reject: rej } = Promise.withResolvers();
-          this.state.editing.canEdit = false;
-          // reset user message scale (on stop)
-          this.state._unsetters.push(() => this._handleScaleConstraint());
-          // set as resolve handler to resolve waiting get features from server
-          this.#startAsync = res;
-          // listen selected attribute
-          this.state._unsetters.push(Vue.watch(() => this.state.selected, () => this._handleScaleConstraint(), { immediate: true }));
-          // await scale set for get features
-          this.#events.push(GUI.getMap().getView().on('change:resolution', debounce(() => this._handleScaleConstraint()), 600));
-          // click to fit zoom scale constraint
-          this.#events.push(
-            GUI.getMap().on('click', e => {
-              if (this.state.selected && !this.state.editing.canEdit) {
-                GUI.getMap().getView().animate(
-                  { duration: 200, center: e.coordinate },
-                  { duration: 200, resolution: getResolutionFromScale(this.state._constraints.scale, GUI.getMapUnits()) || GUI.getMap().getView().getResolution() }
-                );
-              }
-            })
-          );
-          // if click on start toolbox can edit
-          if (this.state.editing.canEdit) {
-            res();
-          }
-          await promise;
-        }
-
-        // reset eventually message
-        if (!this.state._constraints.scale) {
-          GUI.setModal(false);
-        }
-
-        this.#startAsync = null;
-
-        this.setFeaturesOptions({ filter: options.filter });
-        let features;
-
-        const handlerAfterSessionGetFeatures = async promise => {
-          this.emit('start-editing');
-          // set unique fields values
-          await setLayerUniqueFieldValues(this.getId());
-          features = await promise;
-          return features;
-        }
-
-        const is_started = !!this.isSessionStarted();
-        
-
-        //@TODO need to explain better
-        const GIVE_ME_A_NAME = (
-          ApplicationState.ismobile // is mobile
-          && GUI.isMapHidden() // map is not visible (content 100%)
-          && 'vector' === this.state._layerType // is  vector
-        );
-        if (!is_started && GIVE_ME_A_NAME) {
-          this.setEditing(true);
-          const { promise, resolve: res } = Promise.withResolvers();
-          GUI.onceafter('setHidden', () => setTimeout(res, 300));
-          await promise;
-          this.#start = true;
-          this.startLoading();
-          this.setFeaturesOptions({ filter: options.filter });
-          await handlerAfterSessionGetFeatures(this._session.start(this.state._getFeaturesOption));
-        }
-
-        /** @TODO merge the following conditions? */
-        if (!is_started && !GIVE_ME_A_NAME) {
-          this.#start = true;
-          this.startLoading();
-          await handlerAfterSessionGetFeatures(this._session.start(this.state._getFeaturesOption))
-        }
-
-        if (is_started && !this.#start) {
-          this.startLoading();
-          await handlerAfterSessionGetFeatures(this._session.getFeatures(this.state._getFeaturesOption))
-          this.#start = true;
-        }
-
-        if (is_started) { this.setEditing(true); }
-
-        // disablemapcontrols in conflict
-        if (options.disablemapcontrols ?? false) {
-          GUI.disableClickMapControls(true);
-        }
-
-        // wait for features before changing editing layer style 
-        if (this.state.layer.config.editing.layer_style && this.#current_style !== this.state.layer.config.editing.layer_style) {
-          await getCatalogLayerById(this.state.id).changeStyle(this.state.layer.config.editing.layer_style);
-        }
-
-        // force vector layer visibity when starting toolbox (eg. image layers whose catalog layer may be hidden)
-        this.state.layer.getOLLayer?.()?.setVisible(true);
-
-        this.stopLoading();
-        this.setEditing(true);
-
-        resolve({ features });
-      } catch(e) {
-
-        console.warn(e);
-
-        if (!e.signal) {
-          GUI.showUserMessage({ type: 'alert', message: e.message });
-        }
-        
-        this.stop();
-        this.stopLoading();
-        reject(e);
+      // set title
+      if (undefined !== options.title) {
+        this.setTitle(options.title);
       }
 
-    });
+      this.state.changingtools = options.changingtools ?? false;
+
+      if (options.tools) {
+        this.setEnablesDisablesTools(options.tools);
+      }
+
+      this.state.toolboxheader    = options.toolboxheader ?? true;
+      this.state.startstopediting = options.startstopediting ?? true;
+  
+      options.filter = constraints?.filter || this.constraints.filter || options.filter;
+
+      // register lock features to show a message
+      const unKeyLock = this._editor.onceafter('featuresLockedByOtherUser', () => {
+        GUI.showUserMessage({
+          type:     'warning',
+          subtitle: this.state.layer.getName().toUpperCase(),
+          message:  'plugins.editing.messages.featureslockbyotheruser',
+        })
+      });
+  
+      // add featuresLockedByOtherUser setter
+      this.state._unsetters.push(() => this._editor.un('featuresLockedByOtherUser', unKeyLock));
+
+      // check if can we edit based on scale contraint (vector layer)
+      if (this.state._constraints.scale) {
+        const { promise, resolve: res, reject: rej } = Promise.withResolvers();
+        this.state.editing.canEdit = false;
+        // reset user message scale (on stop)
+        this.state._unsetters.push(() => this._handleScaleConstraint());
+        // set as resolve handler to resolve waiting get features from server
+        this.#startAsync = res;
+        // listen selected attribute
+        this.state._unsetters.push(Vue.watch(() => this.state.selected, () => this._handleScaleConstraint(), { immediate: true }));
+        // await scale set for get features
+        this.#events.push(GUI.getMap().getView().on('change:resolution', debounce(() => this._handleScaleConstraint()), 600));
+        // click to fit zoom scale constraint
+        this.#events.push(
+          GUI.getMap().on('click', e => {
+            if (this.state.selected && !this.state.editing.canEdit) {
+              GUI.getMap().getView().animate(
+                { duration: 200, center: e.coordinate },
+                { duration: 200, resolution: getResolutionFromScale(this.state._constraints.scale, GUI.getMapUnits()) || GUI.getMap().getView().getResolution() }
+              );
+            }
+          })
+        );
+        // if click on start toolbox can edit
+        if (this.state.editing.canEdit) {
+          res();
+        }
+        await promise;
+      }
+
+      // reset eventually message
+      if (!this.state._constraints.scale) {
+        GUI.setModal(false);
+      }
+
+      this.#startAsync = null;
+
+      this.setFeaturesOptions({ filter: options.filter });
+      let features;
+
+      const is_started = !!this.isSessionStarted();
+
+      //@TODO need to explain better
+      const GIVE_ME_A_NAME = (
+        ApplicationState.ismobile // is mobile
+        && GUI.isMapHidden() // map is not visible (content 100%)
+        && 'vector' === this.state._layerType // is  vector
+      );
+      if (!is_started && GIVE_ME_A_NAME) {
+        this.setEditing(true);
+        const { promise, resolve: res } = Promise.withResolvers();
+        GUI.onceafter('setHidden', () => setTimeout(res, 300));
+        await promise;
+        this.#start = true;
+        this.startLoading();
+        this.setFeaturesOptions({ filter: options.filter });
+
+        this.emit('start-editing');
+        await setLayerUniqueFieldValues(this.getId());
+        features = await this._session.start(this.state._getFeaturesOption);
+      }
+
+      /** @TODO merge the following conditions? */
+      if (!is_started && !GIVE_ME_A_NAME) {
+        this.#start = true;
+        this.startLoading();
+
+        this.emit('start-editing');
+        await setLayerUniqueFieldValues(this.getId());
+        features = await this._session.start(this.state._getFeaturesOption);
+      }
+
+      if (is_started && !this.#start) {
+        this.startLoading();
+
+        this.emit('start-editing');
+        await setLayerUniqueFieldValues(this.getId());
+        features = await this._session.getFeatures(this.state._getFeaturesOption);
+
+        this.#start = true;
+      }
+
+      if (is_started) {
+        this.setEditing(true);
+      }
+
+      // disablemapcontrols in conflict
+      if (options.disablemapcontrols ?? false) {
+        GUI.disableClickMapControls(true);
+      }
+
+      // wait for features before changing editing layer style 
+      if (this.state.layer.config.editing.layer_style && this.#current_style !== this.state.layer.config.editing.layer_style) {
+        await getCatalogLayerById(this.state.id).changeStyle(this.state.layer.config.editing.layer_style);
+      }
+
+      // force vector layer visibity when starting toolbox (eg. image layers whose catalog layer may be hidden)
+      this.state.layer.getOLLayer?.()?.setVisible(true);
+
+      this.stopLoading();
+      this.setEditing(true);
+
+      return { features };
+    } catch(e) {
+      console.warn(e);
+      if (!e.signal) {
+        GUI.showUserMessage({ type: 'alert', message: e.message });
+      }
+      this.stop();
+      this.stopLoading();
+      throw e;
+    }
   };
 
   /**
