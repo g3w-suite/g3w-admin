ec56c024377c69a58ad3e062e76b825e541a8a47
wlorenzetti <lorenzetti@gis3w.it>
2026-06-17 09:31:12 +0200

    Add missing dependencies for migration 0096 to enable pg_trgm extension

--- g3w-admin/core/migrations/0096_enable_pg_trgm.py
+++ g3w-admin/core/migrations/0096_enable_pg_trgm.py
@@ -4,6 +4,10 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
+    dependencies = [
+        ('core', '0095_generalsuitedata_about_description_es_and_more'),
+    ]
+
     operations = [
         TrigramExtension(),
     ]
