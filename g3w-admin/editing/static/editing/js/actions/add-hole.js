854c518a54a919eaef1dad34dc103e46e865a3e5
Raruto <Raruto@users.noreply.github.com>
2026-09-08 14:03:45 +0200

    refactor(add-hole): dead code

--- g3w-admin/editing/static/editing/js/actions/add-hole.js
+++ g3w-admin/editing/static/editing/js/actions/add-hole.js
@@ -30,32 +30,16 @@ export class AddHoleStep extends Step {
     }
   }
 
-  #coordinatesToGeometry(geometryType = '', coordinates) {
-    const type = String(geometryType).toUpperCase();
-
-    if (type.includes('MULTIPOLYGON')) return new ol.geom.MultiPolygon(coordinates);
-    if (type.includes('POLYGON'))      return new ol.geom.Polygon(coordinates);
-    if (type.includes('MULTILINE'))    return new ol.geom.MultiLineString(coordinates);
-    if (type.includes('LINE'))         return new ol.geom.LineString(coordinates);
-    if (type.includes('MULTIPOINT'))   return new ol.geom.MultiPoint(coordinates);
-    if (type.includes('POINT'))        return new ol.geom.Point(coordinates);
-
-    console.warn('invalid geometry type: ', geometryType);
-    return new ol.geom.Point(coordinates);
-  }
-
   run(inputs, context) {
     return new Promise((resolve, reject) => {
-      this.geometryType = Geometry.getOLGeometry(inputs.layer.getGeometryType());
-      //draw interaction to draw hole on polygon
+      const GEOM_TYPE = Geometry.getOLGeometry(inputs.layer.getGeometryType());
+
+      // draws an hole on polygon
       this.drawInteraction = new ol.interaction.Draw({
         type:              'Polygon',
         source:            new ol.source.Vector(),
         freehandCondition: ol.events.condition.never,
       });
-
-      this.addInteraction(this.drawInteraction);
-      this.drawInteraction.setActive(true);
 
       this.drawInteraction.on('drawstart', ({ feature }) => {
         this.drawingFeature = feature;
@@ -65,7 +49,7 @@ export class AddHoleStep extends Step {
       this.drawInteraction.on('drawend', evt => {
         document.removeEventListener('keydown', this._removeLastPoint);
         // IN CASE OF Z VALUE OF COORDINATE ADD Z VALUE TO COORDINATES OF DRAW POLYGON HOLE
-        if (Geometry.is3DGeometry(this.geometryType)) {
+        if (Geometry.is3DGeometry(GEOM_TYPE)) {
           evt.feature.setGeometry(Geometry.addZValueToOLFeatureGeometry(evt.feature.getGeometry()))
         }
         const hole   = evt.feature;
@@ -73,27 +57,22 @@ export class AddHoleStep extends Step {
         // In case of MultiPolygon
         let newFeature, originalFeature;
 
-        if (Geometry.isMultiGeometry(this.geometryType)) {
+        if (Geometry.isMultiGeometry(GEOM_TYPE)) {
           // cycle on each MultiPolygon feature of layer Multipolygon
-          source
-            .getFeatures()
-            .find(feature => {
-              //feature is a multipolygon
-              //find single polygon of multipolygon that contain draw hole
-              const findPolygonIndex = feature
-                .getGeometry()
-                .getCoordinates()
-                .findIndex(coords => within(this.#coordinatesToGeometry('Polygon', coords), hole.getGeometry()))
-              //if it finds
-              if (findPolygonIndex !== -1) {
-                originalFeature = feature.clone();
-                newFeature = feature;
-                const coordinates = newFeature.getGeometry().getCoordinates();
-                coordinates[findPolygonIndex].push(hole.getGeometry().getCoordinates()[0]);
-                newFeature.getGeometry().setCoordinates(coordinates);
-                return true;
-              }
-            });
+          source.getFeatures().find(feature => {
+            //feature is a multipolygon
+            //find single polygon of multipolygon that contain draw hole
+            const idx = feature.getGeometry().getCoordinates().findIndex(coords => within(new ol.geom.Polygon(coords), hole.getGeometry()))
+            //if it finds
+            if (idx !== -1) {
+              originalFeature = feature.clone();
+              newFeature = feature;
+              const coords = newFeature.getGeometry().getCoordinates();
+              coords[idx].push(hole.getGeometry().getCoordinates()[0]);
+              newFeature.getGeometry().setCoordinates(coords);
+              return true;
+            }
+          });
         } else { // In case of Polygon
           newFeature = source.getFeatures().find(f => within(f.getGeometry(), hole.getGeometry()));
           if (newFeature) {
@@ -113,10 +92,12 @@ export class AddHoleStep extends Step {
           GUI.showUserMessage({ type: 'warning', message: 'No hole is created' });
           reject();
         }
-      })
+      });
 
       this.snapInteraction = new ol.interaction.Snap({ source: getEditingLayer(inputs.layer).getSource() });
 
+      this.addInteraction(this.drawInteraction);
+      this.drawInteraction.setActive(true);
       this.addInteraction(this.snapInteraction);
     })
     
