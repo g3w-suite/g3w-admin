55b8d50bb54b593e70d5127386ebb137aed6725b
volterra79 <boccacci.francesco@gmail.com>
2026-09-22 16:56:56 +0200

    dep(CLIENT-WMTS): first commit

--- g3w-admin/client/static/client/map-controls/geocoding.js
+++ g3w-admin/client/static/client/map-controls/geocoding.js
@@ -762,9 +762,10 @@ document.head.insertAdjacentHTML(
   'beforeend',
   /* css */`
 <style>
-  .ol-geocoder                                                         { width: 300px; margin-top: 3px; margin-left: 5px; --skin-color: #374146; }
+  .ol-geocoder                                                         { width: 300px; margin-top: 3px; margin-left: calc(var(--ml) + 5px) !important; --skin-color: #374146; }
   body:not(.sidebar-collapse) .ol-geocoder,
-  body.is-iframe:not(.sidebar-mini).sidebar-collapse .ol-geocoder      { margin-left: 40px; }
+  body.sidebar-open .ol-geocoder,
+  body.is-iframe:not(.sidebar-mini).sidebar-collapse .ol-geocoder      { margin-left: calc(var(--ml) + 40px) !important; }
   .ol-geocoder > ul                                                    { max-height: 400px; overflow-x: hidden; overflow-y: auto; transition: max-height 300ms ease-in; padding: 0; margin:unset; inset:unset; background: #fff;border: 1px solid #ccc; width: 300px; min-width: 215px; }
   .ol-geocoder > ul:not([style*="left"])                               { position-anchor: --ol-geocoder-form; position-area: bottom; }
   .ol-geocoder > ul > li                                               { width: 100%; overflow: hidden; padding: 0; min-height: 30px; padding-left: 3px; border-bottom: 2px solid var(--skin-color); min-height: 20px; padding: 10px; }
--- g3w-admin/client/static/client/map-controls/queryby.js
+++ g3w-admin/client/static/client/map-controls/queryby.js
@@ -41,6 +41,13 @@ const POLYGON_TYPES = [
 const CONTROLS = {};
 
 /**
+ * Keep child controls out of the map DOM while preserving their interactions.
+ * 
+ * @TODO avoid using inner `new MapControl` instead
+ */
+const FAKE_TARGET = document.createElement('div');
+
+/**
  * Spatial query options
  */
 const QUERY = Vue.observable({
@@ -359,6 +366,7 @@ export class QueryBy extends MapControl {
      */
     const control = CONTROLS[type] = new MapControl({
       name:             type,
+      target:           FAKE_TARGET,
       offline:          false,
       visible:          false,
       geometryTypes:    ['querybypolygon','querybydrawpolygon','querybyfreehand'].includes(type) ? POLYGON_TYPES : [],
@@ -518,7 +526,7 @@ export class QueryBy extends MapControl {
    */
   onSelectLayer(layer) {
 
-    const btn = document.querySelector('.usermessage-content .clear-selected-layer');
+    const btn = document.querySelector('.usermessage-tool .clear-selected-layer');
     if (btn) {
       btn.classList.toggle('hidden', !layer);
     }
