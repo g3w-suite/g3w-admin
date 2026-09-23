4d9bd419e389ab6b1de7da27b24910efeccdaa60
Raruto <Raruto@users.noreply.github.com>
2026-09-23 14:57:29 +0200

    chore: remove `console.log`

--- g3w-admin/editing/static/editing/js/utils/setVertexStyle.js
+++ g3w-admin/editing/static/editing/js/utils/setVertexStyle.js
@@ -26,13 +26,13 @@ export function setVertexStyle({
         )
 
       }),
-      geometry: f => { console.log(f.getGeometry().getCoordinates()); return new ol.geom.MultiPoint(
+      geometry: f => new ol.geom.MultiPoint(
         (/^(Multi)?Polygon/i.test(geometryType) && /^Multi(LineString|Polygon|Point|Line)/i.test(geometryType))
           ? f.getGeometry().getCoordinates().flatMap(coods => coods[0])    // in the case of multipolygon geometry
           : /^(Multi)?Line(String)?/i.test(geometryType)
             ? f.getGeometry().getCoordinates().flat()
             : [f.getGeometry().getCoordinates()]
-      )}
+      )
     }),
     new ol.style.Style({ stroke: new ol.style.Stroke({ color: lineColor, width: strokeWidth })})
   ]);
