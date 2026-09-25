5dc7ed93d6c41b5af56a34aef7064388141adf50
Francesco Boccacci <boccacci.francesco@gmail.com>
2026-09-25 08:42:58 +0200

    [EDITING] Unlock layer after commit add, edit, delete feature within an iframe (#1471)
    * fix(editing-iframe): unlock layer after commit add, edit, delete feature
    
    * fix(iframe-editor): ensure interactions are cleared only if features exist before saving

--- g3w-admin/editing/static/editing/js/g3w-iframe.js
+++ g3w-admin/editing/static/editing/js/g3w-iframe.js
@@ -381,13 +381,10 @@ export class IframeEditor extends Emitter {
             }),
           })
         ).json();
+        
+        //unlock the layer after commit
+        await fetch(`${VECTOR_URL}unlock/${GID}/${qgs_layer_id}/`);
     }
-
-    // unlock layer
-    if (['update', 'delete'].includes(method)) {
-      await fetch(`${VECTOR_URL}unlock/${GID}/${qgs_layer_id}/`);
-    }
-
     // Draw/modify geometry
     if ('draw' === method) {
       GUI.disableClickMapControls(true);
@@ -460,12 +457,12 @@ export class IframeEditor extends Emitter {
 
     // save features (to layer)
     if ('save' === method) {
-      if (layer) {
+      if (layer && layer.getOLLayer().getSource().getFeatures().length > 0) {
         geojson = (new ol.format.GeoJSON()).writeFeatureObject(layer.getOLLayer().getSource().getFeatures()[0]);
         layer.getOLLayer().getSource().clear();
-        this.#interactions.forEach(i => GUI.getMap().removeInteraction(i));
-        this.#interactions = [];
       }
+      this.#interactions.forEach(i => GUI.getMap().removeInteraction(i));
+      this.#interactions = [];
       GUI.disableClickMapControls(false);
     }
 
